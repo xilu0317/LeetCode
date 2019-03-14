6d6e34c634f4cf8e56a30cfc1b6fd381df426910
Xi Lu <xilu0317@gmail.com>
2019-03-14 08:40:07 -0700

    more work

--- test.js
+++ test.js
@@ -1,31 +1,111 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-const searchMin = (nums, lo, hi) => {
-  // Please use Math.floor for javascripot
-  // Avoid (lo+hi)/2 to avoid integer overflow
-  let mid = lo + Math.floor((hi-lo)/2);
+let boolMatrix;
+let good = 0;
 
-  // covers the normal case of `cliff drop`
-  if (nums[mid] > nums[mid+1]) return nums[mid+1];
+const updateNeighbor = (grid, i, j) => {
+  let count = 0;
   
-  // covers the special case of `normal` ascending AFTER handling the `cliff drop`
-  if (mid === 0) return nums[0];
+  // top
+  if (i-1 >= 0 && grid[i-1][j] === 1) {
+    boolMatrix[i-1][j] = true;
+  }
 
-  // note `mid+1 and mid-1` won't work
-  if (nums[mid] > nums[hi]) {
-    return searchMin(nums, mid, hi);
-  } else {
-    return searchMin(nums,lo, mid);
+  // bot
+  if (i+1 < grid.length && grid[i+1][j] === 1) {
+    boolMatrix[i+1][j] = true;
+  }
+  
+  // left
+  if (j-1 >= 0 && grid[i][j-1] === 1) {
+    boolMatrix[i][j-1] = true;
+  }
+  
+  // right
+  if (j+1 < grid[i].length && grid[i][j+1] === 1) {
+    boolMatrix[i][j+1] = true;
   }
 }
 
-const findMin = (nums) => {
-  return searchMin(nums,0, nums.length-1);
-};
+const resetBoolMatrix = (lenRow, lenCol) => {
+  boolMatrix = Array(lenRow).fill().map(()=>Array(lenCol).fill(false))
+}
 
-console.log(findMin([2,1]));//1
-console.log(findMin([1,2,3,4]));//1
-console.log(findMin([2,3,4,5,6,1]));//1
-console.log(findMin([1]));//1
+const isNbAll0 = (grid,i,j) => {
+  let bool1 = true;
+  let bool2 = true;
+  let bool3 = true;
+  let bool4 = true;
+
+  // top
+  if (i-1 >= 0 && grid[i-1][j] !== 0) bool1 = false;
+
+  // bottom
+  if (i+1 < grid.length && grid[i+1][j] !== 0) bool2 = false;
+  
+  // left
+  if (j-1 >= 0 && grid[i][j-1] !== 0) bool3 = false;
+  
+  // right
+  if (j+1 < grid[i].length && grid[i][j+1] !== 0) bool4 = false;
+
+  return bool1 && bool2 && bool3 && bool4;
+}
+
+const existUnreachableNode = (grid) => {
+  let lenRow = grid.length;
+  let lenCol = grid[0].length;
+  for (let i = 0; i < lenRow; i++) {
+    for (let j = 0; j < lenCol; j++) {
+      if (isNbAll0(grid,i,j) && grid[i][j] === 1) {
+        return true;
+      }
+    }
+  }
+}
+
+var orangesRotting = (grid) => {
+  let lenRow = grid.length;
+  let lenCol = grid[0].length;
+  if (existUnreachableNode(grid)) {
+    return -1;
+  }
+
+  resetBoolMatrix(lenRow, lenCol);
+
+  for (var i = 0; i < lenRow; i++) {
+    for (var j = 0; j < lenCol; j++) {
+      if (grid[i][j] === 1) {
+        good++;
+      }
+    }
+  }
+
+  let count = 0;
+  while (good > 0) {
+    for (var i = 0; i < lenRow; i++) {
+      for (var j = 0; j < lenCol; j++) {
+        if (grid[i][j] === 2){
+          updateNeighbor(grid,i,j);
+        }
+      }
+    }
+    
+    good = 0;
+    for (let i = 0; i < lenRow; i++) {
+      for (let j = 0; j < lenCol; j++) {
+        if (boolMatrix[i][j]) {
+          grid[i][j]=2;
+        }
+
+        if(grid[i][j]===1){
+          good++;
+        }
+      }
+    }
+    count++;
+    resetBoolMatrix(lenRow, lenCol);
+  }
+
+  return count;
+}
+
+console.log(orangesRotting([[2,1,0,2]]));
