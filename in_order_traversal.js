80cc3d727e99070a0e1d2af9bb9b66af6eecc16e
Xi Lu <xilu0317@gmail.com>
2019-03-19 00:43:03 -0700

    add more

--- in_order_traversal.js
+++ in_order_traversal.js
@@ -1,25 +1,26 @@
-var inorderTraversal = function(root) {
-  let res = []
-  if (!root) return res;
-  
-  let stack = [];
-  let node = root;
-  let flag = true;
+const inorderTraversal = (root) => {
+  if (!root) return [];
     
+  let res = []
+  let stack = [];
+  let node = root; // node is basically cur, the reference will move
+
   while (true) {
     if (node) {
-      stack.push(node);
-      node = node.left;
+      stack.push(node); // This is like trail blazing, we `record` the path that we have taken already
+      node = node.left; // keep going left as long as we could, note that the leftmost node would the absolute min when the entry point is the root
     } else {
-      if (stack.length) {
-        node = stack.pop()
-        res.push(node.val);
-        node = node.right
+      if (stack.length) {  // The stack.pop() part is the `backtrack` part
+        node = stack.pop() // Cannot add the `let` keyword here, bc of the block scope. It will be problematic when going right in next 2 lines.
+        res.push(node.val); // The `do` or `print` operation
+        node = node.right // rturn from the left, now go right
       } else {
-        break;
+        break; // no `left` to go and stack is empty
       }
     }
   }
-  
+
   return res;
 };
+
+// Think about my original algorithm where `in-order` didn't matter.
