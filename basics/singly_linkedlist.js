7966c672ddc04351d6971d32657a5f0faa9b0420
Xi Lu <xilu0317@gmail.com>
2019-03-30 20:29:08 -0700

    need to work on linkedlist more

---
+++ basics/singly_linkedlist.js
@@ -0,0 +1,24 @@
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const generateListNodeFromArray = (arr) => {
+  let cur = head = new ListNode(0);
+
+  for (let i = 0; i < arr.length; i++) {
+    cur.val = arr[i];
+
+    if (i < arr.length -1) {
+      cur.next = new ListNode(0);
+    }
+
+    cur = cur.next;
+  }
+
+  return head;
+}
+
+module.exports = generateListNodeFromArray;
---
+++ merge_sorted_list.js
@@ -0,0 +1,74 @@
+// 1) make sure the relative path is correct
+// 2) the browser javascript keyword `import` just doesn't work
+const stuff = require('./basics/singly_linkedlist');
+
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const mergeTwoLists = (head1, head2) => {
+  if (!head1 && !head2) return null;
+
+  let cur1 = head1;
+  let cur2 = head2;
+  let head = new ListNode(0);
+  let cur = head;
+
+  if (head1 && head2) {
+    if (head1.val < head2.val) {
+      head.val = head1.val;
+      cur1 = cur1.next;
+    } else {
+      head.val = head2.val;
+      cur2 = cur2.next;
+    }
+  } else {
+    if (!head2) {
+      head.val = head1.val;
+      cur1 = cur1.next;
+    }
+    
+    if (!head1) {
+      head.val = head2.val;
+      cur2 = cur2.next;
+    }
+  }
+
+  while (cur1 && cur2) {
+    if (cur1.val < cur2.val) {
+      cur.next = new ListNode(cur1.val);
+      cur1 = cur1.next;
+    } else {
+      cur.next = new ListNode(cur2.val);
+      cur2 = cur2.next;
+    }
+    cur = cur.next;
+  }
+  
+  while (cur1) {
+    cur.next = new ListNode(cur1.val);
+    cur1 = cur1.next;
+    cur = cur.next;
+  }
+  
+  while (cur2) {
+    cur.next = new ListNode(cur2.val);
+    cur2 = cur2.next;
+    cur = cur.next;
+  }
+  
+  return head;
+};
+
+let head1 = stuff([1]);
+let head2 = stuff([2]);
+let head = mergeTwoLists(head1, head2);
+console.log(head);
