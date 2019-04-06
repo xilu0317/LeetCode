4f5b538c2834fad5fe57a4443026f2dea9de950d
Xi Lu <xilu0317@gmail.com>
2019-04-06 00:58:25 -0700

    this is innovative

---
+++ largeGroupPositions.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} S
+ * @return {number[][]}
+ */
+
+// Maintain two pointers `start` and `finish`
+// Speical processing of the boundary condition where you have 'abbccbbbb'
+// Note there is no way to use the `change of character` to detect the last group
+
+var largeGroupPositions = function(S) {
+  if (!S) return null;
+  
+  let start = 0;
+  let finish = 0;
+  let res = [];
+  let len = S.length;
+  
+  while (finish < len) {
+      if (S[start] === S[finish]) {
+          finish++;
+          // This is for checking the edge condition
+          if (finish === len && finish - 1 - start >=2 ) {
+              res.push([start, finish - 1]);
+          }
+      } else {
+          res.push([start, finish - 1]);
+          start = finish;
+      }
+  }
+  
+  return res.filter(x => x[1] - x[0] >= 2);
+};
