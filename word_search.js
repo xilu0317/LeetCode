0a1c09add8c16427621015599f4bd62d0ff23296
Xi Lu <xilu0317@gmail.com>
2019-04-23 23:14:32 -0700

    new

--- word_search.js
+++ word_search.js
@@ -7,26 +7,34 @@ const exist = (board, word) => {
 
   for (let i = 0; i < rowLen; ++i) {
     for (let j = 0; j < colLen; ++j) {
-      if ( word[0] === board[i][j] 
-           && search(board, word, i, j, 0)) return true;
+      if (word[0] === board[i][j] &&
+          search(board, word, i, j, 0)) return true;
     }
   }
+  
   return false;
 };
 
 const search = (board, word, i, j, index) => {
   if (index === word.length) return true;
 
-  if (i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word[index] || visited[i][j]) return false;
+  // validating boundary edge cases
+  if (i >= board.length            || 
+      i <  0                       || 
+      j >= board[i].length         || 
+      j <  0                       || 
+      board[i][j] !== word[index]  || 
+      visited[i][j]) return false;
 
   visited[i][j] = true;
 
-  if (search(board, word, i - 1, j, index + 1) ||
-      search(board, word, i + 1, j, index + 1) ||
-      search(board, word, i, j - 1, index + 1) ||
-      search(board, word, i, j + 1, index + 1)
+  if (search(board, word, i - 1, j    , index + 1) ||
+      search(board, word, i + 1, j    , index + 1) ||
+      search(board, word, i    , j - 1, index + 1) ||
+      search(board, word, i    , j + 1, index + 1)
      ) return true;
 
+  // set it back to unvisited, if it gets this far and not found in using `search` return false
   visited[i][j] = false;
   return false;
 };
