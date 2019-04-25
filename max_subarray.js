acd215c6effa4ab76a0389701d0efddb649ccb61
Xi Lu <xilu0317@gmail.com>
2019-04-24 23:44:40 -0700

    maxsubarr

---
+++ max_subarray.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSubArray = (nums) => {
+  let dp = Array(nums.length).fill(0);
+  dp[0] = nums[0];
+  let max = dp[0];
+
+  for (let i = 1; i < nums.length; ++i) {
+    dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+    max = Math.max(max, dp[i]);
+  }
+
+  return max;
+};
