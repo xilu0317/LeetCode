77b47e9e33c683f08d52827c2b08a279012bcc45
Xi Lu <xilu0317@gmail.com>
2019-05-05 17:40:13 -0700

    test

--- backtrack/generate_paren.js
+++ backtrack/generate_paren.js
@@ -8,12 +8,17 @@ const generateParenthesis = (n) => {
   return list;
 };
 
+// This is backtrack but however it is less akin to the dfs.
+// It doesn't seem to have the notion of children.
 const backtrack = (list, str, open, close, max) => {
+  // exit condition
   if (str.length === max * 2) {
     list.push(str);
     return;
   }
 
+  // number of open bracket less than the max number of brackets allowed
   if (open < max) backtrack(list, str + '(', open + 1, close, max);
+  // if the number of close bracket is lower than the open bracket recurse
   if (close < open) backtrack(list, str + ')', open, close + 1, max);
 }
