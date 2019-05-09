2c86a472cf7bd31ee60393046a6323d9d0b12bbb
Xi Lu <xilu0317@gmail.com>
2019-05-08 22:18:15 -0700

    test

--- basics/heap.js
+++ basics/heap.js
@@ -1,23 +1,19 @@
+// parent should be smaller than its children
 function MinHeap() {
   this.data = [];
 }
 
 MinHeap.prototype.insert = function(val) {
   this.data.push(val);
-  this.bubbleUp(this.data.length-1);
+  this.bubbleUp(this.data.length - 1);
 };
 
 MinHeap.prototype.bubbleUp = function(index) {
   while (index > 0) {
-    // get the parent
     var parent = Math.floor((index + 1) / 2) - 1;
     
-    // if parent is greater than child
     if (this.data[parent] > this.data[index]) {
-      // swap
-      var temp = this.data[parent];
-      this.data[parent] = this.data[index];
-      this.data[index] = temp;
+      [this.data[parent], this.data[index]] = [this.data[index], this.data[parent]];
     }
     
     index = parent;
@@ -30,7 +26,6 @@ MinHeap.prototype.extractMin = function() {
   // set first element to last element
   this.data[0] = this.data.pop();
   
-  // call bubble down
   this.bubbleDown(0);
   
   return min;
@@ -38,11 +33,10 @@ MinHeap.prototype.extractMin = function() {
 
 MinHeap.prototype.bubbleDown = function(index) {
   while (true) {
-    var child = (index+1)*2;
+    var child = (index + 1 ) * 2;
     var sibling = child - 1;
     var toSwap = null;
     
-    // if current is greater than child
     if (this.data[index] > this.data[child]) {
       toSwap = child;
     }
@@ -57,10 +51,8 @@ MinHeap.prototype.bubbleDown = function(index) {
       break;
     }
     
-    var temp = this.data[toSwap];
-    this.data[toSwap] = this.data[index];
-    this.data[index] = temp;
-    
+    [this.data[toSwap], this.data[index]] = [this.data[index], this.data[toSwap]];
+
     index = toSwap;
   }
 };
