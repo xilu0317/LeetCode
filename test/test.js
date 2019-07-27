0a1c141d57d6f56c65a74136b15dbacef1328dec
Xi Lu <xilu0317@gmail.com>
2019-07-26 20:55:23 -0700

    new

--- test/test.js
+++ test/test.js
@@ -1,13 +1,24 @@
 (function () {
+    // test
     let str = 'abcdeABEG';
+    console.log(fun(str));
 
-
-    let arr = Array.from(str);    
-    for (let i = 0; i < 10; i++) {
-        console.log(arr[i]);
+    function isLowerCase(c) {
+        return c === c.toLowerCase();
     }
 
-
-
-
+    function fun(str) {
+        let arr = [...str];    
+    
+        let lowArr = arr.filter(x => isLowerCase(x)).sort();
+        let upperArr = arr.filter(x => !isLowerCase(x)).sort();
+    
+        for (let i = upperArr.length - 1; i >= 0; i--) {
+            for (let j = lowArr.length - 1; j >= 0; j--) {
+                if (upperArr[i].toLowerCase() === lowArr[j]) return upperArr[i];
+            }
+        }
+    
+        return '-1';
+    }
 })();
