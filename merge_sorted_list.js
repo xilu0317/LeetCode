bdc2ba834c7daebe93746fbb716ad244f25a9308
Xi Lu <xilu0317@gmail.com>
2019-07-27 16:18:59 -0700

    new

--- merge_sorted_list.js
+++ merge_sorted_list.js
@@ -14,63 +14,30 @@ class ListNode {
  * @param {ListNode} l2
  * @return {ListNode}
  */
-const mergeTwoListsIterative = (head1, head2) => {
-    if (!head1 && !head2) return null;
+const mergeTwoLists = (head1, head2) => {
+    // Note the use of sentenial aka `dummy` here
+	let dummy = new ListNode(Number.MAX_SAFE_INTEGER);
+	let cur = dummy;
 
-    let cur1 = head1;
-    let cur2 = head2;
-    let head = new ListNode(0);
-    let cur = head;
+	while (head1 && head2) {
+		if (head1.val < head2.val) {
+			cur.next = head1;
+			head1 = head1.next;
+		} else {
+			cur.next = head2;
+			head2 = head2.next;
+		}
+		cur = cur.next;
+	}
 
-    if (head1 && head2) {
-        if (head1.val < head2.val) {
-            head.val = head1.val;
-            cur1 = cur1.next;
-        } else {
-            head.val = head2.val;
-            cur2 = cur2.next;
-        }
-    } else {
-        if (!head2) {
-            head.val = head1.val;
-            cur1 = cur1.next;
-        }
+	cur.next = head1 ? head1 : head2;
 
-        if (!head1) {
-            head.val = head2.val;
-            cur2 = cur2.next;
-        }
-    }
-
-    while (cur1 && cur2) {
-        if (cur1.val < cur2.val) {
-            cur.next = new ListNode(cur1.val);
-            cur1 = cur1.next;
-        } else {
-            cur.next = new ListNode(cur2.val);
-            cur2 = cur2.next;
-        }
-        cur = cur.next;
-    }
-
-    while (cur1) {
-        cur.next = new ListNode(cur1.val);
-        cur1 = cur1.next;
-        cur = cur.next;
-    }
-
-    while (cur2) {
-        cur.next = new ListNode(cur2.val);
-        cur2 = cur2.next;
-        cur = cur.next;
-    }
-
-    return head;
+	return dummy.next;
 };
 
 const mergeTwoLists = (head1, head2) => { 
-	if (head1 === null) return head2;
-	if (head2 === null) return head1;
+	if (!head1) return head2;
+	if (!head2) return head1;
 
 	if (head1.val < head2.val) {
 		head1.next = mergeTwoLists(head1.next, head2);
@@ -79,7 +46,7 @@ const mergeTwoLists = (head1, head2) => {
 		head2.next = mergeTwoLists(head1, head2.next);
 		return head2;
 	}
-}
+};
 
 let head1 = stuff([1]);
 let head2 = stuff([2]);
