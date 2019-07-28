b964fcbeefa3ed1a03854775ba898e18b91b0ecf
Xi Lu <xilu0317@gmail.com>
2019-07-27 21:38:13 -0700

    new

--- merge_sorted_list.js
+++ merge_sorted_list.js
@@ -16,40 +16,41 @@ class ListNode {
  */
 const mergeTwoLists = (head1, head2) => {
     // Note `dummy` is just an auxiliary placeholder
-	let dummy = new ListNode(Number.MAX_SAFE_INTEGER);
-	let cur = dummy;
+    let dummy = new ListNode(Number.MAX_SAFE_INTEGER);
+    let cur = dummy;
 
-	while (head1 && head2) {
-		if (head1.val < head2.val) {
-			cur.next = head1;
-			head1 = head1.next;
-		} else {
-			cur.next = head2;
-			head2 = head2.next;
-		}
-		cur = cur.next;
-	}
+    while (head1 && head2) {
+        if (head1.val < head2.val) {
+            cur.next = head1;
+            head1 = head1.next;
+        } else {
+            cur.next = head2;
+            head2 = head2.next;
+        }
+        cur = cur.next;
+    }
 
     // Handle the leftover linked list
-	cur.next = head1 ? head1 : head2;
+    cur.next = head1 ? head1 : head2;
 
     // Note it is *not* returning `dummy` but `dummy.next`
-	return dummy.next;
+    return dummy.next;
 };
 
-const mergeTwoLists = (head1, head2) => { 
-	if (!head1) return head2;
-	if (!head2) return head1;
+const mergeTwoLists = (head1, head2) => {
+    if (!head1) return head2;
+    if (!head2) return head1;
 
-	if (head1.val < head2.val) {
-		head1.next = mergeTwoLists(head1.next, head2);
-		return head1;
-	} else {
-		head2.next = mergeTwoLists(head1, head2.next);
-		return head2;
-	}
+    if (head1.val < head2.val) {
+        head1.next = mergeTwoLists(head1.next, head2);
+        return head1;
+    } else {
+        head2.next = mergeTwoLists(head1, head2.next);
+        return head2;
+    }
 };
 
+// [Test case]
 let head1 = stuff([1]);
 let head2 = stuff([2]);
 let head = mergeTwoLists(head1, head2);
