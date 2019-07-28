6cb37c798bb9177ee3f73caa1ceac08032b0883a
Xi Lu <xilu0317@gmail.com>
2019-07-27 21:54:47 -0700

    new

--- wildcard_matching.js
+++ wildcard_matching.js
@@ -6,10 +6,10 @@
 
 const isMatch = (str, pattern) => {
 	// s = index for str
+	// sMatch = index for save pos of str
 	// p = index for pattern
-
-	// pStar = index for the star
-	let s = 0, p = 0, match = 0, pStar = -1;
+	// pStar = index for the star of pattern
+	let s = 0, p = 0, sMatch = 0, pStar = -1;
 
 	while (s < str.length) {
 		if ((pattern[p] === '?' || pattern[p] === str[s]) && p < pattern.length) {
@@ -18,11 +18,11 @@ const isMatch = (str, pattern) => {
 		} else if (pattern[p] === '*' && p < pattern.length) {
 			pStar = p;
 			p++;
-			match = s; // Save the current pos of the string
+			sMatch = s; // Save the current pos of the string
 		} else if (pStar !== -1) { // If the previous pattern index is `*`
 			p = pStar + 1; // Then advance string pointer
-			match++;
-			s = match; // Extract the next pos of the string
+			sMatch++;
+			s = sMatch; // Extract the next pos of the string
 		} else {
 			return false;
 		}
