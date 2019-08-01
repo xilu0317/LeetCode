54de7bd70cbb9f62e7028da0c2615bd28d5f83d7
Xi Lu <xilu0317@gmail.com>
2019-07-31 21:07:13 -0700

    new

---
+++ OS_MS/bitdiff.js
@@ -0,0 +1,24 @@
+//  depending on the definition
+const hammingWeight = (n) => {
+    return [ ... n.toString(2)].filter(x => x === '1').length;
+};
+
+const bitDiff = (a, b) => {
+	return Math.abs(hammingWeight(a) - hammingWeight(b));
+};
+
+const bitDiff2 = (a, b) => {
+	let aStr = a.toString(2);
+	let bStr = b.toString(2);
+
+	let len = a > b ? a : b;
+
+	aStr = aStr.padStart(len, '0');
+	bStr = bStr.padStart(len, '0');
+
+	return aStr^bStr;
+};
+
+
+
+
