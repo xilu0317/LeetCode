7a7e33628857598f6a7fd22d2034749c74fbf2e4
Xi Lu <xilu0317@gmail.com>
2019-08-01 21:08:07 -0700

    new

--- OS_MS/merge_interal.js
+++ OS_MS/merge_interal.js
@@ -3,33 +3,38 @@ const merge = (intervals) => {
 
 	if (intervals.length === 0 || intervals.length === 1) return intervals;
 
-	intervals.sort((a,b) => a[0] - b[0]);
+	// sorting is the key
+	intervals.sort((a, b) => a[0] - b[0]);
 
+	// init arr
 	let res = [];
-	let blob = [];
+	let merged = [];
+	// add first element so the rest have something to compare
 	let firstInterval = intervals[0];
-	blob.push(firstInterval[0]);
-	blob.push(firstInterval[1]);
+	merged.push(firstInterval[0]);
+	merged.push(firstInterval[1]);
 
 	for (let i = 1; i < intervals.length; ++i) {
-	  let interval = intervals[i];
-	  let start = interval[0];
-	  let end = interval[1];
+		let interval = intervals[i];
+		let start = interval[0];
+		let end = interval[1];
 
-	  if (start <= blob[blob.length - 1]) {
-		blob.push(end > blob[blob.length - 1] ? end : blob[blob.length - 1])
-	  } else {
-		res.push([ blob[0], blob[blob.length - 1] ]);
-		blob = [];
-		blob.push(start);
-		blob.push(end);
-	  }
+		if (start <= merged[merged.length - 1]) {
+			merged.push(end > merged[merged.length - 1] ? end : merged[merged.length - 1])
+		} else {
+			res.push([merged[0], merged[merged.length - 1]]);
+			// re-init the merged array
+			merged = [];
+			merged.push(start);
+			merged.push(end);
+		}
 
-	  if ( i === intervals.length - 1) {
-		res.push([ blob[0], blob[blob.length - 1] ]);
-	  }
+		// handle the last pari
+		if (i === intervals.length - 1) {
+			res.push([merged[0], merged[merged.length - 1]]);
+		}
 
 	}
 
 	return res;
-  };
+};
