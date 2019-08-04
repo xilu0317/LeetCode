36da8a5063d83b6933451942d60a900e52a0d9af
Xi Lu <xilu0317@gmail.com>
2019-08-04 13:09:56 -0700

    new

--- OS_MS/lru.js
+++ OS_MS/lru.js
@@ -1,29 +1,29 @@
 class LRUCache {
-	constructor(capacity) {
-		this.capacity = capacity;
-		this.map = new Map();
-	}
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.map = new Map();
+    }
 
-	get(key) {
-		let val = this.map.get(key);
+    get(key) {
+        let val = this.map.get(key);
 
-		if (typeof val === 'undefined') return -1;
+        if (!val) return -1;
 
-		this.map.delete(key);
-		this.map.set(key, val);
+        this.map.delete(key);
+        this.map.set(key, val);
 
-		return val;
-	}
+        return val;
+    }
 
-	put(key, value) {
-		if (this.map.has(key)) {
-			this.map.delete(key);
-		}
-		this.map.set(key, value);
+    put(key, value) {
+        if (this.map.has(key)) {
+            this.map.delete(key);
+        }
+        this.map.set(key, value);
 
-		let keys = this.map.keys();
-		while (this.map.size > this.capacity) {
-			this.map.delete(keys.next().value);
-		}
-	}
+        let keys = this.map.keys();
+        while (this.map.size > this.capacity) {
+            this.map.delete(keys.next().value);
+        }
+    }
 }
