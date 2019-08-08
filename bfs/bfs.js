74ba1e7aa8afe80340a6f1aedc7d1cd6b243ab3d
Xi Lu <xilu0317@gmail.com>
2019-08-08 08:52:09 -0700

    new

--- bfs/bfs.js
+++ bfs/bfs.js
@@ -5,11 +5,15 @@ class Node {
     }
 }
 
+// bfs node-based underlying map
 const bfs = (root) => {
     if (!root) return;
 
+    // put the root into the queue
     let q = [root];
+    // keep the loop going when the queue is not empty
     while (q.length) {
+        // this is only needed for level by level exploration
         let len = q.length;
         for (let i = 0; i < len; ++i) {
             let node = q.shift(); // dequeue
@@ -19,4 +23,13 @@ const bfs = (root) => {
         }
     }
 };
+
+// TODO:
+// bfs - adjancecy list
+
+// TODO:
+// bfs - adjancecy matrix
+
+// TODO:
+// add a more universal bfs version down below (no level by level exploration scheme)
 
