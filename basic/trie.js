ad698e8fab41479d790b6284cbab81fea7362a15
Xi Lu <xilu0317@gmail.com>
2019-08-09 21:51:54 -0700

    new

--- basic/trie.js
+++ basic/trie.js
@@ -1,36 +1,3 @@
-// Old function object style
-function Trie() {
-	const root = {};
-	
-    return { insert, search, startsWith };
-    
-    function insert(word) {
-        let cur = root;
-        word.split('').forEach(ch => cur = cur[ch] = cur[ch] || {});
-        cur.isWord = true;
-    }
-    
-    function traverse(word) {
-        let cur = root;
-        for (let i = 0; i < word.length; i++) {
-            if (!cur) return null;
-            cur = cur[word[i]];
-		}
-		
-        return cur;
-    }
-    
-    function search(word) {
-		let node = traverse(word);
-		
-        return !!node && !!node.isWord;
-    }
-    
-    function startsWith(word) {
-        return !!traverse(word);
-    }
-}
-
 // ES6 class
 class Trie {
 	constructor() {
@@ -64,5 +31,35 @@ class Trie {
 	}
 }
 
-
-
+// Old function object style
+function Trie() {
+	const root = {};
+	
+    return { insert, search, startsWith };
+    
+    function insert(word) {
+        let cur = root;
+        word.split('').forEach(ch => cur = cur[ch] = cur[ch] || {});
+        cur.isWord = true;
+    }
+    
+    function traverse(word) {
+        let cur = root;
+        for (let i = 0; i < word.length; i++) {
+            if (!cur) return null;
+            cur = cur[word[i]];
+		}
+		
+        return cur;
+    }
+    
+    function search(word) {
+		let node = traverse(word);
+		
+        return !!node && !!node.isWord;
+    }
+    
+    function startsWith(word) {
+        return !!traverse(word);
+    }
+}
