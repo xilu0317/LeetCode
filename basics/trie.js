0f78e1ac48e0c86b21bf0410091b4d7311cfeeab
Xi Lu <xilu0317@gmail.com>
2019-08-09 22:54:54 -0700

    new

--- basics/trie.js
+++ basics/trie.js
@@ -22,14 +22,14 @@ class Trie {
             if (!cur) return null;
             cur = cur[c];
 		}
-		
+
         return cur;
 	}
 
 	search(word) {
         // if the last node exists and the terminal flag is true, then the word is found
 		let lastNode = this.traverse(word);
-		
+
         // `!!` is a common pratice to booleaniz variables
         return !!lastNode && !!lastNode.isWord;
 	}
@@ -42,31 +42,31 @@ class Trie {
 // Old function object style
 function Trie() {
 	const root = {};
-	
+
     return { insert, search, startsWith };
-    
+
     function insert(word) {
         let cur = root;
         word.split('').forEach(ch => cur = cur[ch] = cur[ch] || {});
         cur.isWord = true;
     }
-    
+
     function traverse(word) {
         let cur = root;
         for (let i = 0; i < word.length; i++) {
             if (!cur) return null;
             cur = cur[word[i]];
 		}
-		
+
         return cur;
     }
-    
+
     function search(word) {
 		let node = traverse(word);
-		
+
         return !!node && !!node.isWord;
     }
-    
+
     function startsWith(word) {
         return !!traverse(word);
     }
