640acc12c95194271571958f5aada9007aa382d6
Xi Lu <xilu0317@gmail.com>
2019-08-12 14:21:29 -0700

    new

---
+++ binaryTree/is_valid_bst.js
@@ -0,0 +1,31 @@
+const getMax = (root) => {
+    if (!root) throw "The root node cannot be null!";
+
+    while (root.right) {
+        root = root.right
+    }
+
+    return root.val;
+};
+
+const getMin = (root) => {
+    if (!root) throw "The root node cannot be null!";
+
+    while (root.left) {
+        root = root.left;
+    }
+
+    return root.val;
+};
+
+const isValidBST = (root) => {
+    if (!root) return true;
+
+    if (!root.left && !root.right) return true;
+
+    if (!root.left) return isValidBST(root.right) && root.val < getMin(root.right);
+
+    if (!root.right) return isValidBST(root.left) && root.val > getMax(root.left);
+
+    return isValidBST(root.left) && isValidBST(root.right) && getMax(root.left) < root.val && root.val < getMin(root.right);
+};
