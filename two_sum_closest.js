e46cb79bf40a5faa8312126f7deb71dab9900783
Xi Lu <xilu0317@gmail.com>
2019-08-20 21:09:09 -0700

    new

---
+++ two_sum_closest.js
@@ -0,0 +1,20 @@
+const twoSumClosest = (nums, target) => {
+    nums.sort((a, b) => a > b);
+    let min = Number.MAX_SAFE_INTEGER;
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        if (nums[left] + nums[right] < target) {
+            min = Math.min(min, target - nums[left] - nums[right]);
+            left++;
+        } else {
+            min = Math.min(min, nums[left] + nums[right] - target);
+            right--;
+        }
+    }
+
+    return min;
+};
+
+console.log(twoSumClosest([1, 2, 3, 7, 10], 6));
