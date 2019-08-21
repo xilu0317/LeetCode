69a33310aad933ed80105ab645e19400bfd23813
Xi Lu <xilu0317@gmail.com>
2019-08-20 21:08:44 -0700

    new

--- my_fav/num_islands.js
+++ my_fav/num_islands.js
@@ -1,43 +1,27 @@
-/**
- * @param {character[][]} grid
- * @return {number}
- */
+let n, m;
 
-// Share m, n
-let m, n;
-
-// helper
 const dfsMarking = (grid, i, j) => {
-    if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] === '0') {
-        return;
-    }
-
-    // Flag '1' => '0'
-    grid[i][j] = '0';
-
-    // Recursive dfs
-    dfsMarking(grid, i + 1, j);
-    dfsMarking(grid, i - 1, j);
-    dfsMarking(grid, i, j + 1);
-    dfsMarking(grid, i, j - 1);
+	if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] === '0') return;
+	grid[i][j] = '0';
+	dfsMarking(grid, i + 1, j);
+	dfsMarking(grid, i - 1, j);
+	dfsMarking(grid, i, j + 1);
+	dfsMarking(grid, i, j - 1);
 };
 
-// main method
 const numIslands = (grid) => {
-    let count = 0;
-    n = grid.length;
-    m = grid[0].length;
+	let count = 0;
+	n = grid.length;
+	if (n === 0) return 0;
 
-    if (n === 0) return 0;
+	m = grid[0].length;
+	for (let i = 0; i < n; i++) {
+		for (let j = 0; j < m; j++)
+			if (grid[i][j] === '1') {
+				dfsMarking(grid, i, j);
+				count++;
+			}
+	}
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++)
-            // if node is '1' mark reachable neighbors '0' and increment the count
-            if (grid[i][j] === '1') {
-                dfsMarking(grid, i, j);
-                count++;
-            }
-    }
-
-    return count;
+	return count;
 };
