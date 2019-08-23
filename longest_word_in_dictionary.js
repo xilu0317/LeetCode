45642339b97bb6ce5edb282120ef411e1eec2b2a
Xi Lu <xilu0317@gmail.com>
2019-08-22 21:33:19 -0700

    new

---
+++ longest_word_in_dictionary.js
@@ -0,0 +1,52 @@
+const comp = (a, b) => {
+    if (a.length > b.length) {
+        return -1;
+    }
+    if (a.length < b.length) {
+        return 1;
+    }
+
+    return a.localeCompare(b);
+};
+
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+const longestWord = (words) => {
+    let s = new Set();
+
+    // add to set
+    for (let word of words) {
+        s.add(word);
+    }
+
+    // build tire
+    words.sort(comp);
+    const root = {};
+    for (const word of words) {
+        let cur = root;
+        for (const c of word) {
+            cur[c] = cur[c] || {};
+        }
+    }
+
+    // traverse trie
+    let strArr = [];
+    for (const word of words) {
+        let cur = root;
+        let str = '';
+        for (const c of word) {
+            if (cur[c]) {
+                str += c;
+                if (!s.has(str)) {
+                    str = '';
+                    break;
+                }
+            }
+        }
+        strArr.push(str);
+    }
+
+    return strArr.sort(comp)[0];
+};
--- word_search.js
+++ word_search.js
@@ -1,10 +1,14 @@
 let visited;
+let rowLen;
+let colLen;
 
 const exist = (board, word) => {
-    let rowLen = board.length;
-    let colLen = board[0].length;
+    rowLen = board.length;
+    colLen = board[0].length;
+    // Underlying visit map is to mark the visit status
     visited = Array(rowLen).fill().map(x => Array(colLen).fill(false));
 
+    // Use every point as a starting point
     for (let i = 0; i < rowLen; ++i) {
         for (let j = 0; j < colLen; ++j) {
             if (search(board, word, i, j, 0)) return true;
@@ -14,27 +18,32 @@ const exist = (board, word) => {
     return false;
 };
 
+// why a grid needs to be marked as visisted?
+// This is because that it is like the visit flag which will allow the algorithm to avoid redundent visits
+
 const search = (board, word, i, j, index) => {
     if (index === word.length) return true;
 
-    // validating boundary edge cases
-    if (i < 0 || i >= board.length ||
-        j < 0 || j >= board[i].length ||
+    // Validating boundary edge cases
+    if (i < 0 || i >= rowLen ||
+        j < 0 || j >= colLen ||
         board[i][j] !== word[index] ||
         visited[i][j]) return false;
 
-    // mark as visited
+    // Mark as visited
     visited[i][j] = true;
 
+    // Recursively visits all grids
     if (search(board, word, i - 1, j, index + 1) ||
         search(board, word, i + 1, j, index + 1) ||
         search(board, word, i, j - 1, index + 1) ||
         search(board, word, i, j + 1, index + 1)
     ) return true;
 
-    // unmark as unvisited 
+
+    // Mark as unvisited
     visited[i][j] = false;
 
-    // if it gets this far and not found in using `search` return false
+    // If it gets this far and not found in using `search` return false
     return false;
 };
