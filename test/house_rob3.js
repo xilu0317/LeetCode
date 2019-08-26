28e201f649bddcafbf84ad3b334b963bdc7f6ebc
Xi Lu <xilu0317@gmail.com>
2019-08-25 20:49:10 -0700

    new

--- test/house_rob3.js
+++ test/house_rob3.js
@@ -1,15 +1,16 @@
 const dfs = (x) => {
-	if (!x) return [null, null];
+    if (!x) return [null, null];
 
-	let left = dfs(x.left);
-	let right = dfs(x.right);
+    let left = dfs(x.left);
+    let right = dfs(x.right);
 
-	return [left[1] + right[1] + x.val, Math.max(left[0], left[1]) + Math.max(right[0], right[1])];
+    return [left[1] + right[1] + x.val, Math.max(left[0], left[1]) + Math.max(right[0], right[1])];
 };
 
 const rob = (root) => {
-	let num = dfs(root);
+    if (!root) return 0;
 
-	return Math.max(num[0], num[1]);
+    let nums = dfs(root);
+
+    return Math.max(nums[0], nums[1]);
 };
-
