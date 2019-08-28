7c6328064aff03858c305e61890d3ad756a9d929
Xi Lu <xilu0317@gmail.com>
2019-08-27 21:12:16 -0700

    new

---
+++ zigzag_traversal.js
@@ -0,0 +1,37 @@
+const stuff = require('../basics/bst.js');
+
+let root = stuff([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+zigzagWalk = (root) => {
+    if (!root) return;
+
+    let flag = true;
+    let levelList = [];
+    let q = [root];
+
+    while (q.length) {
+        let len = q.length;
+        for (let i = 0; i < len; i++) {
+            let node = q.shift();
+            // don't requeue the null nodes
+            if (node) {
+                levelList.push(node.val);
+                q.push(node.left);
+                q.push(node.right);
+            }
+        }
+
+        if (flag) {
+            levelList.forEach(x => console.log(x));
+        } else {
+            levelList.reverse().forEach(x => console.log(x))
+        }
+        // change flag
+        flag = !flag;
+        // clear out the level list
+        levelList = [];
+    }
+
+}
+
+zigzagWalk(root);
