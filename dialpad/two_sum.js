c75885543d6bbbfdd2ee128bad3864e5a66dae54
Xi Lu <xilu0317@gmail.com>
2019-08-27 21:16:50 -0700

    new

---
+++ dialpad/two_sum.js
@@ -0,0 +1,15 @@
+const twoSum = (nums, target) => {
+	let dict = {};
+	for (let i = 0; i < nums.length; i++) {
+		dict[nums[i]] = i;
+	}
+
+	for (let i = 0; i < nums.length; i++) {
+		let goal = target - nums[i];
+		if (dict[goal] && i !== dict[goal]) {
+			return [i, dict[goal]];
+		}
+	}
+
+	return -1;
+};
