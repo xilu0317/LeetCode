ed94fdd29a9f8b84177635a13dbd300837077814
Xi Lu <xi.lu@sophos.com>
2019-08-28 11:56:57 -0700

    test

--- my_fav/num_islands.js
+++ my_fav/num_islands.js
@@ -1,27 +1,31 @@
+// This is so to share `m` and `n` between two methods
 let n, m;
 
 const dfsMarking = (grid, i, j) => {
-	if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] === '0') return;
-	grid[i][j] = '0';
-	dfsMarking(grid, i + 1, j);
-	dfsMarking(grid, i - 1, j);
-	dfsMarking(grid, i, j + 1);
-	dfsMarking(grid, i, j - 1);
+    if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] === '0') return;
+
+    grid[i][j] = '0';
+
+    dfsMarking(grid, i + 1, j);
+    dfsMarking(grid, i - 1, j);
+    dfsMarking(grid, i, j + 1);
+    dfsMarking(grid, i, j - 1);
 };
 
 const numIslands = (grid) => {
-	let count = 0;
-	n = grid.length;
-	if (n === 0) return 0;
+    n = grid.length;
+    m = grid[0].length;
 
-	m = grid[0].length;
-	for (let i = 0; i < n; i++) {
-		for (let j = 0; j < m; j++)
-			if (grid[i][j] === '1') {
-				dfsMarking(grid, i, j);
-				count++;
-			}
-	}
+    if (n === 0) return 0;
 
-	return count;
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++)
+            if (grid[i][j] === '1') {
+                dfsMarking(grid, i, j);
+                count++;
+            }
+    }
+
+    return count;
 };
