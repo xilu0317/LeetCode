493dd35c118cc9b737f44603f07ed03bf779e8f3
Xi Lu <xi.lu@sophos.com>
2019-09-02 08:58:37 -0700

    test

--- my_fav/num_islands.js
+++ my_fav/num_islands.js
@@ -3,7 +3,7 @@ let m, n;
 const dfsEraseLand = (grid, i, j) => {
     // If current grid is out of bound or on water
     // then exit the funciton immeidately
-    if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] === '0') return;
+    if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === '0') return;
 
     // Erase the current land grid by marking it as water
     grid[i][j] = '0';
@@ -17,19 +17,19 @@ const dfsEraseLand = (grid, i, j) => {
 
 /*
     The gist is that go visit each grid in the matrix.
-    If the grid is a land grid then erase all neighoring girds and 
+    If the grid is a land grid then erase all neighoring girds and
     then increment the counter by just 1 for the entire 'landmass'
 */
 const numIslands = (grid) => {
     // The spec requires to return 0 for invalid inputs
     if (!grid || !grid.length) return 0;
 
-    n = grid.length;
-    m = grid[0].length;
+    m = grid.length;
+    n = grid[0].length;
 
     let count = 0;
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
             // If the current grid is a land, erase ALL neighboring landds
             if (grid[i][j] === '1') {
                 dfsEraseLand(grid, i, j);
