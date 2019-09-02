f8ddfc5024b9a88dc52695004ef965ff9d407870
Xi Lu <xilu0317@gmail.com>
2019-09-01 17:12:29 -0700

    new

--- skyline.js
+++ skyline.js
@@ -1,44 +1,129 @@
-const getSkyline = (buildings) => {
-    let res = [], height = [], pq = [0], prevMax = null;
+/**
+ * @param {number[][]} buildings
+ * @return {number[][]}
+ */
 
-    for (const b of buildings) {
-        height.push([b[0], -b[2]]);
-        height.push([b[1], b[2]]);
+class MaxHeap {
+    constructor() {
+        this.data = [null];
     }
-    height.sort((a, b) => {
-        if (a[0] === b[0]) return a[1] - b[1];
-        return a[0] - b[0];
+
+    swap(i, j) {
+        const tmpNode = this.data[i];
+        this.data[i] = this.data[j];
+        this.data[j] = tmpNode;
+    }
+
+    addNode(node) {
+        this.data.push(node);
+        if (this.data.length === 2) {
+            return;
+        }
+        else {
+            this.bubbleUp(this.data.length - 1);
+        }
+    }
+
+    removeMax() {
+        if (this.data === 1) {
+            return null;
+        }
+        else {
+            this.swap(this.data.length - 1, 1);
+            const ret = this.data.pop();
+            this.bubbleDown(1);
+        }
+    }
+
+    bubbleDown(i) {
+        const largestChild = this.data[i * 2 + 1] && this.data[i * 2 + 1].h > this.data[i * 2].h ? i * 2 + 1 : i * 2;
+        if (this.data[largestChild] && this.data[largestChild].h > this.data[i].h) {
+            this.swap(largestChild, i);
+            this.bubbleDown(largestChild);
+        }
+    }
+
+    bubbleUp(i) {
+        const parentIndex = Math.floor(i / 2);
+        if (this.data[parentIndex] && this.data[parentIndex].h < this.data[i].h) {
+            this.swap(parentIndex, i);
+            this.bubbleUp(parentIndex);
+        }
+    }
+
+    deleteNode(node) {
+        const nodeIndex = this.data.indexOf(node);
+        if (nodeIndex === -1) {
+            return false;
+        }
+        else if (nodeIndex === this.data.length - 1) {
+            this.data.pop();
+        }
+        else {
+            this.swap(this.data.length - 1, nodeIndex);
+            this.data.pop();
+            const parentIndex = Math.floor(nodeIndex / 2);
+            if (this.data[parentIndex] && this.data[parentIndex].h < this.data[nodeIndex].h) {
+                this.bubbleUp(nodeIndex);
+            }
+            else {
+                this.bubbleDown(nodeIndex);
+            }
+        }
+    }
+
+    getMaxHeight() {
+        if (this.data.length === 1) {
+            return 0;
+        }
+        else {
+            return this.data[1].h;
+        }
+    }
+}
+
+const comp = (s1, s2) => {
+    if (s1.x !== s2.x) {
+        return s1.x - s2.x;
+    }
+    else if (s1.type !== s2.type) {
+        return s1.type - s2.type;
+    }
+    else if (s1.type === 1) {
+        return s1.h - s2.h;
+    }
+    else {
+        return s1.start.h - s2.start.h;
+    }
+}
+
+const getSkyline = (buildings) => {
+    const buildingHeap = new MaxHeap();
+    const skylines = [];
+    let ret = [];
+
+    buildings.forEach(b => {
+        const start = { type: 1, x: b[0], h: b[2] };
+        const end = { type: 2, x: b[1], start: start };
+        skylines.push(start, end);
     });
 
-    for (const h of height) {
-        if (h[1] < 0) {
-            pq.push(-h[1]);
+    skylines.sort(comp);
+
+    skylines.forEach(skyline => {
+        if (skyline.type === 1) {
+            if (skyline.h > buildingHeap.getMaxHeight()) {
+                ret = ret.filter(r => r[0] !== skyline.x);
+                ret.push([skyline.x, skyline.h]);
+            }
+            buildingHeap.addNode(skyline);
         } else {
-            remove(pq, h[1]);
+            buildingHeap.deleteNode(skyline.start);
+            if (skyline.start.h > buildingHeap.getMaxHeight()) {
+                ret.push([skyline.x, buildingHeap.getMaxHeight()]);
+            }
         }
+    });
 
-        let maxV = Math.max(...pq);
-        // maxV changed after remove h[1]
-        if (prevMax !== maxV) {
-            res.push([h[0], maxV]);
-            prevMax = maxV;
-        }
-    }
-
-    return res;
-};
-
-// remove the first element equal to val
-const remove = (arr, val) => {
-    let index = -1;
-    for (let i = 0; i < arr.length; i++) {
-        if (val === arr[i]) {
-            index = i;
-            break;
-        }
-    }
-
-    arr.splice(index, 1);
-
-    return;
+    return ret;
 };
