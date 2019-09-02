b7a47b5924676459f8dc9524260ad5082514393a
Xi Lu <xilu0317@gmail.com>
2019-09-01 23:42:37 -0700

    new

--- increase_order_search_tree.js
+++ increase_order_search_tree.js
@@ -1,13 +1,40 @@
+// when cur is at the root it doesnt' have next
+//
+//                  root
+//               /        \
+//              /          \
+//          cur.left     cur.right
 const increasingBST = (root) => {
+
     return increasingBSTRec(root, null);
 };
 
-const increasingBSTRec = (root, next) => {
-    if (!root) return next;
+// In terms of naming, it is better to use `cur` instead of `root` to reflect that it is a generic node
+const increasingBSTRec = (cur, next) => {
+    if (!cur) return next;
 
-    const res = increasingBSTRec(root.left, root);
-    root.left = null;
-    root.right = increasingBSTRec(root.right, next);
+    const res = increasingBSTRec(cur.left, cur);
+    cur.left = null;
+    cur.right = increasingBSTRec(cur.right, next);
 
     return res;
 };
+
+//
+//  `next` really is the next node to be processed
+//
+//                      next
+//                      /
+//                     /
+//                 -------
+//                 | cur |
+//                 -------    
+//               /        \
+//              /          \
+//          cur.left     cur.right
+//
+//
+//
+// Recall the very definition of the BST
+// The subtree on the left is recursively smaller than the current node and subtree on the right is recursively larger
+// Thus the value of `cur` is smaller than the value of `next` because cur is the root of the left subtree.
