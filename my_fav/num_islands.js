12553ef8842e8fbb341fac2f1b457d95a985472a
Xi Lu <xi.lu@sophos.com>
2019-09-02 08:42:29 -0700

    test

--- my_fav/num_islands.js
+++ my_fav/num_islands.js
@@ -1,7 +1,8 @@
 let n, m;
 
 const dfsEraseLand = (grid, i, j) => {
-    // If out of bound or on water, then ignore the rest of the code
+    // If current grid is out of bound or on water
+    // then exit the funciton immeidately
     if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] === '0') return;
 
     // Turn current land/grid into water
@@ -15,6 +16,7 @@ const dfsEraseLand = (grid, i, j) => {
 };
 
 const numIslands = (grid) => {
+    // Spec requires to return 0 for invalide inputs
     if (!grid || !grid.length) return 0;
 
     n = grid.length;
@@ -23,11 +25,11 @@ const numIslands = (grid) => {
     let count = 0;
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < m; j++) {
-            // If the current grid is a land, erase all lands nearby
+            // If the current grid is a land, erase ALL neighboring landds
             if (grid[i][j] === '1') {
                 dfsEraseLand(grid, i, j);
                 // Don't forget to count after removing the lands
-                count++;
+                ++count;
             }
         }
     }
