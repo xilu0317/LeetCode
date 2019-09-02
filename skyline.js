2e15f1359f3f55b59efda69dfb2c078c0134066c
Xi Lu <xilu0317@gmail.com>
2019-09-01 17:18:56 -0700

    new

--- skyline.js
+++ skyline.js
@@ -9,17 +9,14 @@ class MaxHeap {
     }
 
     swap(i, j) {
-        const tmpNode = this.data[i];
-        this.data[i] = this.data[j];
-        this.data[j] = tmpNode;
+        [this.data[i], this.data[j]] = [this.data[j], this.data[i]];
     }
 
     addNode(node) {
         this.data.push(node);
         if (this.data.length === 2) {
             return;
-        }
-        else {
+        } else {
             this.bubbleUp(this.data.length - 1);
         }
     }
@@ -27,16 +24,15 @@ class MaxHeap {
     removeMax() {
         if (this.data === 1) {
             return null;
-        }
-        else {
+        } else {
             this.swap(this.data.length - 1, 1);
-            const ret = this.data.pop();
             this.bubbleDown(1);
         }
     }
 
     bubbleDown(i) {
-        const largestChild = this.data[i * 2 + 1] && this.data[i * 2 + 1].h > this.data[i * 2].h ? i * 2 + 1 : i * 2;
+        const largestChild = this.data[i * 2 + 1] &&
+                             this.data[i * 2 + 1].h > this.data[i * 2].h ? i * 2 + 1 : i * 2;
         if (this.data[largestChild] && this.data[largestChild].h > this.data[i].h) {
             this.swap(largestChild, i);
             this.bubbleDown(largestChild);
@@ -55,18 +51,15 @@ class MaxHeap {
         const nodeIndex = this.data.indexOf(node);
         if (nodeIndex === -1) {
             return false;
-        }
-        else if (nodeIndex === this.data.length - 1) {
+        } else if (nodeIndex === this.data.length - 1) {
             this.data.pop();
-        }
-        else {
+        } else {
             this.swap(this.data.length - 1, nodeIndex);
             this.data.pop();
             const parentIndex = Math.floor(nodeIndex / 2);
             if (this.data[parentIndex] && this.data[parentIndex].h < this.data[nodeIndex].h) {
                 this.bubbleUp(nodeIndex);
-            }
-            else {
+            } else {
                 this.bubbleDown(nodeIndex);
             }
         }
@@ -75,8 +68,7 @@ class MaxHeap {
     getMaxHeight() {
         if (this.data.length === 1) {
             return 0;
-        }
-        else {
+        } else {
             return this.data[1].h;
         }
     }
@@ -95,12 +87,12 @@ const comp = (s1, s2) => {
     else {
         return s1.start.h - s2.start.h;
     }
-}
+};
 
 const getSkyline = (buildings) => {
     const buildingHeap = new MaxHeap();
     const skylines = [];
-    let ret = [];
+    let res = [];
 
     buildings.forEach(b => {
         const start = { type: 1, x: b[0], h: b[2] };
@@ -113,17 +105,19 @@ const getSkyline = (buildings) => {
     skylines.forEach(skyline => {
         if (skyline.type === 1) {
             if (skyline.h > buildingHeap.getMaxHeight()) {
-                ret = ret.filter(r => r[0] !== skyline.x);
-                ret.push([skyline.x, skyline.h]);
+                res = res.filter(r => r[0] !== skyline.x);
+                res.push([skyline.x, skyline.h]);
             }
+
             buildingHeap.addNode(skyline);
         } else {
             buildingHeap.deleteNode(skyline.start);
+            
             if (skyline.start.h > buildingHeap.getMaxHeight()) {
-                ret.push([skyline.x, buildingHeap.getMaxHeight()]);
+                res.push([skyline.x, buildingHeap.getMaxHeight()]);
             }
         }
     });
 
-    return ret;
+    return res;
 };
