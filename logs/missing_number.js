10d943522e442a38bdfd76a6e5b66419598acb13
Xi Lu <xilu0317@gmail.com>
2019-09-03 22:44:34 -0700

    new

--- logs/missing_number.js
+++ logs/missing_number.js
@@ -6,6 +6,7 @@ const missingNumber = (nums) => {
     if (!nums) return null;
 
     let res = 0;
+    // The reason that the len here is nums.length + 1 is because the index + 1 can be a number
     for (let i = 0; i < nums.length + 1; ++i) {
         res ^= (i ^ nums[i]);
     }
