d7787b280449a52ef673b758ae024b518f3d4059
Xi Lu <xilu0317@gmail.com>
2019-09-05 21:29:16 -0700

    new

---
+++ buy_sell_stock_2.js
@@ -0,0 +1,24 @@
+// Say you have an array for which the ith element is the price of a given stock on day i.
+// Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+// Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+// Input: [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+//              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+// Input: [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+const maxProfit = (prices) => {
+	let total = 0;
+
+	for (let i = 0; i < prices.length - 1; i++) {
+		if (prices[i + 1] > prices[i]) {
+			total += prices[i + 1] - prices[i];
+		}
+	}
+
+	return total;
+};
