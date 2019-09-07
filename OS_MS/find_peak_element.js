08f61a5323f6ecccafb3d9cad84df516c0670c03
Xi Lu <xilu0317@gmail.com>
2019-09-07 13:25:20 -0700

    new

---
+++ OS_MS/find_peak_element.js
@@ -0,0 +1,24 @@
+const findPeak = (A, left, right) => {
+	// Exit conditions
+	// This is kinda whacky because when the left and right are shrinking into
+	// two elements they need special handling, otherwise it will always return
+	// the previous element of the two because of the nature of Math.floor
+	if (Math.abs(left - right) <= 1) {
+		return A[left] > A[right] ? left : right;
+	}
+
+	mid = Math.floor((left + right) / 2);
+
+	// typical case
+	if (A[mid - 1] < A[mid] && A[mid] > A[mid + 1]) {
+		return mid;
+	} else if (A[mid - 1] < A[mid] && A[mid] < A[mid + 1]) {
+		return findPeak(A, mid + 1, right);
+	} else {
+		return findPeak(A, left, mid - 1);
+	}
+};
+
+const findPeakElement = (A) => {
+	return findPeak(A, 0, A.length - 1);
+};
