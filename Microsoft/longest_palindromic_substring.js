82ec2d16f416b3924de9a37bcd9c9f2bc768d32b
Xi Lu <xilu0317@gmail.com>
2019-09-08 12:31:18 -0700

    new

--- Microsoft/longest_palindromic_substring.js
+++ Microsoft/longest_palindromic_substring.js
@@ -1,11 +1,12 @@
-// This is a very classical dp problem.
-// The key to finding the solution to this problem is to construct the recurrence relationship.
+// The key is to construct the recurrence relationship.
 
 // dp(i, j) represents whether s(i ... j) can form a palindromic substring
 
 // dp(i, j) is true when s(i) equals to s(j) AND s(i + 1 ... j - 1)/enclosd string is palindromic.
 
-// When we found a palindrome, check if it's the longest one. Time complexity O(n^2).
+// When we have found a palindrome, check if it's the longest.
+
+// Time complexity O(n^2).
 
 const longestPalindrome = (s) => {
     const N = s.length;
@@ -14,14 +15,17 @@ const longestPalindrome = (s) => {
     const dp = Array(N).fill()
                        .map(() => Array(N).fill(false));
 
+    // grow the palindrome from back to front
     for (let i = N - 1; i >= 0; i--) {
         for (let j = i; j < N; j++) {
-            if ((s[i] === s[j]) && (j - i <= 2 || dp[i + 1][j - 1])) {
+            if ((s[i] === s[j]) && (dp[i + 1][j - 1]) || j - i <= 2) {
                 dp[i][j] = true;
             }
 
-            // If it is palindromic and the length is greater than the previous length
+            // 'j - i + 1' is the length of the current substring
+            // Watch out for off-by-one error: ie 9 - 0 + 1 = 10 elements in total
             if (dp[i][j] && j - i + 1 > res.length) {
+                // substring bounded by i, j inclusive
                 res = s.substring(i, j + 1);
             }
         }
