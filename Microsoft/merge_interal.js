f7db299a6e1cbb598243b89b0ef4b6fb2d6c9982
Xi Lu <xilu0317@gmail.com>
2019-09-07 23:52:40 -0700

    new

--- Microsoft/merge_interal.js
+++ Microsoft/merge_interal.js
@@ -1,14 +1,20 @@
+// cur => current interval
+// prev => previous interval
+
 const merge = (intervals) => {
-    if (!intervals || !intervals.length) return intervals;
+    if (!intervals || !intervals.length) return [];
 
     // key: sort by starting time
     intervals.sort((a, b) => a[0] - b[0]);
 
     let prev = intervals[0];
+    // put the first interval into `res`
     const res = [prev];
 
     for (const cur of intervals) {
+        // If the starting position of the current is less than or equal to the ending of the previous
         if (cur[0] <= prev[1]) {
+            // then update the previous ending to be whichever to be larger
             prev[1] = Math.max(prev[1], cur[1])
         } else {
             res.push(cur);
