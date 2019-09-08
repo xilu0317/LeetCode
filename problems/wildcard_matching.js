bc680e14ca894ec7b83237ff47e1600d62de3f0c
Xi Lu <xilu0317@gmail.com>
2019-09-08 08:03:43 -0700

    new

--- problems/wildcard_matching.js
+++ problems/wildcard_matching.js
@@ -5,33 +5,33 @@
  */
 
 const isMatch = (str, pattern) => {
-	// s = index for str
-	// sMatch = index for save pos of str
-	// p = index for pattern
-	// pStar = index for the star of pattern
-	let s = 0, p = 0, sMatch = 0, pStar = -1;
+    // s = index for str
+    // sMatch = index for save pos of str
+    // p = index for pattern
+    // pStar = index for the star of pattern
+    let s = 0, p = 0, sMatch = 0, pStar = -1;
 
-	while (s < str.length) {
-		if ((pattern[p] === '?' || pattern[p] === str[s]) && p < pattern.length) {
-			s++;
-			p++;
-		} else if (pattern[p] === '*' && p < pattern.length) {
-			pStar = p;
-			p++;
-			sMatch = s; // Save the current pos of the string
-		} else if (pStar !== -1) { // If the previous pattern index is `*`
-			p = pStar + 1; // Then advance string pointer
-			sMatch++;
-			s = sMatch; // Extract the next pos of the string
-		} else {
-			return false;
-		}
-	}
+    while (s < str.length) {
+        if ((pattern[p] === '?' || pattern[p] === str[s]) && p < pattern.length) {
+            s++;
+            p++;
+        } else if (pattern[p] === '*' && p < pattern.length) {
+            pStar = p;
+            p++;
+            sMatch = s; // Save the current pos of the string
+        } else if (pStar !== -1) { // If the previous pattern index is `*`
+            p = pStar + 1; // Then advance string pointer
+            sMatch++;
+            s = sMatch; // Extract the next pos of the string
+        } else {
+            return false;
+        }
+    }
 
-	// Check for remaining characters in the pattern
-	while (p < pattern.length && pattern[p] === '*') {
-		p++;
-	}
+    // Check for remaining characters in the pattern
+    while (p < pattern.length && pattern[p] === '*') {
+        p++;
+    }
 
-	return (p === pattern.length);
+    return (p === pattern.length);
 };
