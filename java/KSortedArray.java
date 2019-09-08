0c9ff23ba718e15117f6eafd3c5c1935d9875fed
Xi Lu <xilu0317@gmail.com>
2019-09-08 11:45:11 -0700

    new

--- java/KSortedArray.java
+++ java/KSortedArray.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 // Design an array container is the key to solving this problem
 class ArrayContainer {
@@ -16,9 +17,8 @@ class ArrayContainer {
 
 public class KSortedArray {
     public static int[] mergeKSortedArray(int[][] arr) {
-        // specify how comparison of array containers is to be made
         Comparator<ArrayContainer> comp = (ac1, ac2) -> {
-            // compare items of an array with current index
+            // compare items of an array with dynamic index
             if (ac1.arr[ac1.index] < ac2.arr[ac2.index])
                 return -1;
             else if (ac1.arr[ac1.index] == ac2.arr[ac2.index])
@@ -28,9 +28,9 @@ public class KSortedArray {
         };
 
         // key
-        PriorityQueue<ArrayContainer> q = new PriorityQueue<ArrayContainer>(comp);
+        Queue<ArrayContainer> q = new PriorityQueue<ArrayContainer>(comp);
 
-        // Initialization
+        // Initial loading
         int len = 0;
         for (int i = 0; i < arr.length; i++) {
             // queue all ACs in for the first time when their indices are all at '0'
@@ -63,6 +63,7 @@ public class KSortedArray {
         int[] arr3 = { 0, 9, 10, 11 };
 
         int[] result = mergeKSortedArray(new int[][] { arr1, arr2, arr3 });
+
         System.out.println(Arrays.toString(result));
     }
 }
