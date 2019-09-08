ea032eb9f70ed69360e20143435667f8a2b7425c
Xi Lu <xilu0317@gmail.com>
2019-09-07 23:48:09 -0700

    new

--- Microsoft/merge_interal.js
+++ Microsoft/merge_interal.js
@@ -1,12 +1,13 @@
 const merge = (intervals) => {
     if (!intervals || !intervals.length) return intervals;
 
-    intervals.sort((a, b) => a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]);
+    // key: sort by starting time
+    intervals.sort((a, b) => a[0] - b[0]);
 
     let prev = intervals[0];
-    let res = [prev];
+    const res = [prev];
 
-    for (let cur of intervals) {
+    for (const cur of intervals) {
         if (cur[0] <= prev[1]) {
             prev[1] = Math.max(prev[1], cur[1])
         } else {
