43ed9d8afc07e0077608cf372f5473fa1794590b
Xi Lu <xilu0317@gmail.com>
2019-09-07 23:45:42 -0700

    new

--- Microsoft/merge_interal.js
+++ Microsoft/merge_interal.js
@@ -1,39 +1,19 @@
 const merge = (intervals) => {
-	if (!intervals) return null;
+    if (!intervals || !intervals.length) return intervals;
 
-	if (intervals.length === 0 || intervals.length === 1) return intervals;
+    intervals.sort((a, b) => a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]);
 
-	// *Important* sort based on the starting time
-	intervals.sort((a, b) => a[0] - b[0]);
+    let prev = intervals[0];
+    let res = [prev];
 
-	let res = [];
-	let merged = [];
-	// add first element so the rest have something to compare
-	let firstInterval = intervals[0];
-	merged.push(firstInterval[0]);
-	merged.push(firstInterval[1]);
+    for (let cur of intervals) {
+        if (cur[0] <= prev[1]) {
+            prev[1] = Math.max(prev[1], cur[1])
+        } else {
+            res.push(cur);
+            prev = cur;
+        }
+    }
 
-	for (let i = 1; i < intervals.length; ++i) {
-		let interval = intervals[i];
-		let start = interval[0];
-		let end = interval[1];
-
-		if (start <= merged[merged.length - 1]) {
-			merged.push(end > merged[merged.length - 1] ? end : merged[merged.length - 1])
-		} else {
-			res.push([merged[0], merged[merged.length - 1]]);
-			// re-init the merged array
-			merged = [];
-			merged.push(start);
-			merged.push(end);
-		}
-
-		// handle the last pari
-		if (i === intervals.length - 1) {
-			res.push([merged[0], merged[merged.length - 1]]);
-		}
-
-	}
-
-	return res;
+    return res;
 };
