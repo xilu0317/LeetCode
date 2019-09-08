a2ab6522322f97e72214ae719d1575b2b684181e
Xi Lu <xilu0317@gmail.com>
2019-09-08 10:53:30 -0700

    new

--- java/KSortedArray.java
+++ java/KSortedArray.java
@@ -3,64 +3,64 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class ArrayContainer implements Comparator<ArrayContainer> {
-	int[] arr;
-	int index;
+    int[] arr;
+    int index;
 
-	public ArrayContainer(int[] arr, int index) {
-		this.arr = arr;
-		this.index = index;
-	}
+    public ArrayContainer(int[] arr, int index) {
+        this.arr = arr;
+        this.index = index;
+    }
 }
 
 public class KSortedArray {
-	public static int[] mergeKSortedArray(int[][] arr) {
-		Comparator<ArrayContainer> comp = new Comparator<ArrayContainer>() {
-			@Override
-			public int compare(ArrayContainer ac1, ArrayContainer ac2) {
-				if (ac1.arr[ac1.index] < ac2.arr[ac2.index])
-					return -1;
-				else if (ac1.arr[ac1.index] == ac2.arr[ac2.index])
-					return 0;
-				else
-					return 1;
-			}
-		};
+    public static int[] mergeKSortedArray(int[][] arr) {
+        Comparator<ArrayContainer> comp = new Comparator<ArrayContainer>() {
+            @Override
+            public int compare(ArrayContainer ac1, ArrayContainer ac2) {
+                if (ac1.arr[ac1.index] < ac2.arr[ac2.index])
+                    return -1;
+                else if (ac1.arr[ac1.index] == ac2.arr[ac2.index])
+                    return 0;
+                else
+                    return 1;
+            }
+        };
 
-		// Key to solve this problme is to use a PQ
-		PriorityQueue<ArrayContainer> q = new PriorityQueue<ArrayContainer>(comp);
+        // Key to solve this problme is to use a PQ
+        PriorityQueue<ArrayContainer> q = new PriorityQueue<ArrayContainer>(comp);
 
-		// Add array to priority queue
-		int total = 0;
-		for (int i = 0; i < arr.length; i++) {
-			// Note all array containers will have starting index of 0
-			q.add(new ArrayContainer(arr[i], 0));
-			total += arr[i].length;
-		}
+        // Add array to priority queue
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            // Note all array containers will have starting index of 0
+            q.add(new ArrayContainer(arr[i], 0));
+            total += arr[i].length;
+        }
 
-		int m = 0;
-		int result[] = new int[total];
+        int m = 0;
+        int result[] = new int[total];
 
-		// while heap is not empty
-		while (!q.isEmpty()) {
-			ArrayContainer ac = q.poll();
-			result[m++] = ac.arr[ac.index];
+        // while heap is not empty
+        while (!q.isEmpty()) {
+            ArrayContainer ac = q.poll();
+            result[m++] = ac.arr[ac.index];
 
-			if (ac.index < ac.arr.length - 1) {
-				q.add(new ArrayContainer(ac.arr, ac.index + 1));
-			}
-		}
+            if (ac.index < ac.arr.length - 1) {
+                q.add(new ArrayContainer(ac.arr, ac.index + 1));
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	// Test code here:
-	public static void main(String[] args) {
-		int[] arr1 = { 1, 3, 5, 7 };
-		int[] arr2 = { 2, 4, 6, 8 };
-		int[] arr3 = { 0, 9, 10, 11 };
+    // Test code here:
+    public static void main(String[] args) {
+        int[] arr1 = { 1, 3, 5, 7 };
+        int[] arr2 = { 2, 4, 6, 8 };
+        int[] arr3 = { 0, 9, 10, 11 };
 
-		int[] result = mergeKSortedArray(new int[][] { arr1, arr2, arr3 });
-		System.out.println(Arrays.toString(result));
-	}
+        int[] result = mergeKSortedArray(new int[][] { arr1, arr2, arr3 });
+        System.out.println(Arrays.toString(result));
+    }
 
 }
