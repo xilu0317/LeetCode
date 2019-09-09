a8076cf839e2ddc650284cf37925f59aa22eb400
Xi Lu <xilu0317@gmail.com>
2019-09-08 22:45:51 -0700

    new

--- Microsoft/max_sub_array.js
+++ Microsoft/max_sub_array.js
@@ -1,14 +1,17 @@
 const maxSubArray = (nums) => {
     if (!nums || !nums.length) return -1;
 
-    const len = nums.length;
-    const dp = Array(len).fill(0);
+    const LEN = nums.length;
+
+    const dp = Array(LEN).fill(0);
     dp[0] = nums[0];
 
     let max = dp[0];
 
-    for (let i = 1; i < len; i++) {
+    // note it starts at 1
+    for (let i = 1; i < LEN; i++) {
         dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+        // update max using current dp value
         max = Math.max(max, dp[i]);
     }
 
