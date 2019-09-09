2bea3db82a6a614c0fac97556d27bab32d31a668
Xi Lu <xilu0317@gmail.com>
2019-09-08 23:23:11 -0700

    new

--- Microsoft/three_sum.js
+++ Microsoft/three_sum.js
@@ -3,17 +3,22 @@ const threeSum = (nums) => {
     nums.sort((a, b) => a - b);
 
     // Note the right end index is nums.length - 2
-    const res = [];
+    let res = [];
+
     for (let i = 0; i < nums.length - 2; i++) {
         if (i === 0 || nums[i] !== nums[i - 1]) {
-            let lo = i + 1, hi = nums.length - 1, sum = 0 - nums[i];
+            let lo = i + 1;
+            let hi = nums.length - 1;
+            let sum = 0 - nums[i];
+
             while (lo < hi) {
                 if (nums[lo] + nums[hi] === sum) {
                     res.push([nums[i], nums[lo], nums[hi]]);
                     // handle equal keys
                     while (lo < hi && nums[lo] === nums[lo + 1]) lo++;
                     while (lo < hi && nums[hi] === nums[hi - 1]) hi--;
-                    lo++; hi--;
+                    lo++;
+                    hi--;
                 }
                 // two pointer problem - like two sum closest
                 else if (nums[lo] + nums[hi] < sum) {
