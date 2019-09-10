21f314e926fb4f686d2228e4ec5dbbfdbea6b03f
Xi Lu <xilu0317@gmail.com>
2019-09-09 19:03:48 -0700

    new

--- Microsoft/sort_colors.js
+++ Microsoft/sort_colors.js
@@ -1,26 +1,26 @@
 const sortColors = (nums) => {
-	let dict = {};
-	dict[0] = 0;
-	dict[1] = 0;
-	dict[2] = 0;
+    let dict = {};
+    dict[0] = 0;
+    dict[1] = 0;
+    dict[2] = 0;
 
-	for (let i = 0; i < nums.length; ++i) {
-		if (nums[i] === 0)++dict[0];
-		if (nums[i] === 1)++dict[1];
-		if (nums[i] === 2)++dict[2];
-	}
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) dict[0]++;
+        if (nums[i] === 1) dict[1]++;
+        if (nums[i] === 2) dict[2]++;
+    }
 
-	let len0 = dict[0];
-	let len1 = dict[1];
-	let len2 = dict[2];
+    let len0 = dict[0];
+    let len1 = dict[1];
+    let len2 = dict[2];
 
-	for (let i = 0; i < len0; ++i) {
-		nums[i] = 0;
-	}
-	for (let i = len0; i < len0 + len1; ++i) {
-		nums[i] = 1;
-	}
-	for (let i = len0 + len1; i < len0 + len1 + len2; ++i) {
-		nums[i] = 2;
-	}
+    for (let i = 0; i < len0; i++) {
+        nums[i] = 0;
+    }
+    for (let i = len0; i < len0 + len1; i++) {
+        nums[i] = 1;
+    }
+    for (let i = len0 + len1; i < len0 + len1 + len2; i++) {
+        nums[i] = 2;
+    }
 };
