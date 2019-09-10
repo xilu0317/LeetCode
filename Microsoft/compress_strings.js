138359e54abe3326d37bd8cafb3b3a82cf33a077
Xi Lu <xilu0317@gmail.com>
2019-09-09 20:48:06 -0700

    new

--- Microsoft/compress_strings.js
+++ Microsoft/compress_strings.js
@@ -2,10 +2,12 @@ const compress = (chars) => {
     let index = 0; i = 0;
 
     while (i < chars.length) {
+        // get the current char
         let cur = chars[i];
 
+        // re-init count to zero
         let count = 0;
-        while (i < chars.length && chars[i] === cur) {
+        while (chars[i] === cur) {
             i++;
             count++;
         }
