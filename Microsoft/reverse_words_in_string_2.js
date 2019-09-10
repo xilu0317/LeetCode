8d3718fd8fdc75fa8eca4c315f8d6284817abdb4
Xi Lu <xilu0317@gmail.com>
2019-09-10 00:01:31 -0700

    new

--- Microsoft/reverse_words_in_string_2.js
+++ Microsoft/reverse_words_in_string_2.js
@@ -4,6 +4,7 @@
 // Input: ["t", "h", "e", " ", "s", "k", "y", " ", "i", "s", " ", "b", "l", "u", "e"]
 // Output: ["b", "l", "u", "e", " ", "i", "s", " ", "s", "k", "y", " ", "t", "h", "e"]
 
+// Questions to clarify with the interviewers
 // A word is defined as a sequence of non - space characters.
 // The input string does not contain leading or trailing spaces.
 // The words are always separated by a single space.
