a71a83d4677fb7de62a2360d0f79af473dd61349
Xi Lu <xi.lu@sophos.com>
2019-09-10 15:16:58 -0700

    new

--- Microsoft/rotate_array.js
+++ Microsoft/rotate_array.js
@@ -4,7 +4,7 @@ const rotate = (nums, k) => {
 
     // must do mod thing here to acount for over rotation
     let len = nums.length;
-    k = k % len;
+    k %= len;
     if (k === 0) return nums;
 
     reverse(nums, 0, len - 1);
@@ -17,7 +17,7 @@ const reverse = (nums, start, end) => {
     while (start < end) {
         // remember to use the latest syntax when swapping
         [nums[start], nums[end]] = [nums[end], nums[start]];
-        ++start;
-        --end;
+        start++;
+        end--;
     }
 };
