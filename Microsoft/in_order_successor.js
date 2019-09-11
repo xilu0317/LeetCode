24d062dec302b3e586de387fa6d68e6b52e80eb2
Xi Lu <xilu0317@gmail.com>
2019-09-10 21:40:47 -0700

    new

--- Microsoft/in_order_successor.js
+++ Microsoft/in_order_successor.js
@@ -1,20 +1,59 @@
+// Central idea is to inspect the current node and determine where to go. If the cur val is
+// smaller than or equal the target value we should go to the right side
+
+// When puzzled always draw a diagram to assist the understanding
+//
+//                  CUR
+//                /
+//               /
+//              P
+//            /   \
+//           /     \
+//       Subtree   Subtree <=== The successor must be here
+//           *Case I
+//
+//
+//                  CUR
+//                /
+//               /
+//              P
+//            /   \
+//           /     \
+//       Subtree   NULL
+//           *Case II
+//
+//
+//                  Larger (Because of Case III, P can't really be here, should have already returned at this point)
+//                /
+//               /
+//             CUR
+//            /   \
+//           /     \
+//       Smaller   Larger (P)  <=== Since P in this subtree, direct the code to go search the right subtree
+//           *Case III
+//
+// Using 'cur' is preferred because it reflects the dynamic nature of the 'root'
 /**
  * @param {TreeNode} root
  * @param {TreeNode} p
  * @return {TreeNode}
  */
-// cur is the entry point, normally root.
 const inorderSuccessor = (cur, p) => {
+    // Exit conition for recursion
     if (!cur) return null;
 
-    if (cur.val <= p.val) {
-        return inorderSuccessor(cur.right, p);
-    } else {
+    if (cur.val > p.val) {
+        // If the current value is greater than the p value, go search on the left subtree if nothing exisit the current value is the succeor
         return inorderSuccessor(cur.left, p) || cur;
+        //                 *Case I           *Case II
+    } else {
+        // If the current value is smaller, go search on the right subtree
+        return inorderSuccessor(cur.right, p);
+        //                 *Case III
     }
 };
 
-// Nothing speical to it. All symmetrical.
+// Nothing speical to the predecessor code. All symmetrical.
 const predecessor = (cur, p) => {
     if (!cur) return null;
 
