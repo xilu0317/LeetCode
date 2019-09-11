643e8fe023817831e451d753933b1ab84ef699f0
Xi Lu <xilu0317@gmail.com>
2019-09-11 08:39:18 -0700

    new

--- Microsoft/meeting_rooms.js
+++ Microsoft/meeting_rooms.js
@@ -5,16 +5,15 @@
 // TODO: revisit
 const minMeetingRooms = (intervals) => {
     let len = intervals.length;
-    let starts = Array(len).fill(0);
-    let ends = Array(len).fill(0);
+    let starts = [];
+    let ends = [];
 
-    for (let i = 0; i < len; i++) {
-        // '0' means start
-        starts[i] = intervals[i][0];
-        // '1' means end
-        ends[i] = intervals[i][1];
+    for (let interval of intervals) {
+        starts.push(interval[0]);
+        ends.push(interval[1]);
     }
 
+    // KEY
     starts.sort((a, b) => a - b);
     ends.sort((a, b) => a - b);
 
