62301123bae793d3985bddafde54fc564be9b72d
Xi Lu <xilu0317@gmail.com>
2019-09-11 09:00:45 -0700

    new

--- Microsoft/compare_versions.js
+++ Microsoft/compare_versions.js
@@ -1,9 +1,12 @@
 const compareVersion = (v1, v2) => {
+    // use parseInt for numerical comparison
     let list1 = v1.split(/\./).map(x => parseInt(x));
     let list2 = v2.split(/\./).map(x => parseInt(x));
 
-    let minLen = Math.min(list1.length, list2.length)
-    let maxLen = Math.max(list1.length, list2.length);
+    let len1 = list1.length, len2 = list2.length
+
+    let minLen = Math.min(len1, len2)
+    let maxLen = Math.max(len1, len2);
 
     // define 'i' outside the for loop to obtain more scope
     let i;
@@ -16,6 +19,7 @@ const compareVersion = (v1, v2) => {
         }
     }
 
+    // don't forget to increment 'i'
     while (i < maxLen) {
         if (list1[i] > 0) {
             return 1;
