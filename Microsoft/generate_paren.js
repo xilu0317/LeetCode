dd833710106381fa6a1d418e6ffe17c9c4af0753
Xi Lu <xi.lu@sophos.com>
2019-09-11 15:45:35 -0700

    new

--- Microsoft/generate_paren.js
+++ Microsoft/generate_paren.js
@@ -4,24 +4,26 @@
  */
 
 const generateParenthesis = (n) => {
-    let list = [];
-    backtrack(list, '', 0, 0, n);
+    let res = [];
+    backtrack(res, '', 0, 0, n);
 
-    return list;
+    return res;
 };
 
 // This is backtrack but however it is less akin to the dfs.
 // It doesn't seem to have the notion of children.
-const backtrack = (list, str, numOpen, numClose, max) => {
+const backtrack = (res, str, numOpen, numClose, max) => {
     // exit condition
     if (str.length === max * 2) {
-        list.push(str);
+        res.push(str);
 
         return;
     }
 
     // number of numOpen bracket less than the max number of brackets allowed
-    if (numOpen < max) backtrack(list, str + '(', numOpen + 1, numClose, max);
+    if (numOpen < max)
+        backtrack(res, str + '(', numOpen + 1, numClose, max);
     // if the number of close bracket is lower than the numOpen bracket recurse
-    if (numClose < numOpen) backtrack(list, str + ')', numOpen, numClose + 1, max);
+    if (numClose < numOpen)
+        backtrack(res, str + ')', numOpen, numClose + 1, max);
 };
