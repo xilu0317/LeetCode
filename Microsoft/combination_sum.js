0c77ad43e759bc36b14d9b6447f2330cacb4f713
Xi Lu <xilu0317@gmail.com>
2019-09-10 20:34:55 -0700

    new

--- Microsoft/combination_sum.js
+++ Microsoft/combination_sum.js
@@ -3,8 +3,11 @@
  * @param {number} target
  * @return {number[][]}
  */
+
 const combinationSum = (candidates, target) => {
     let res = [];
+
+    // Important! Make sure the numbers are sorted
     candidates.sort((a, b) => a - b);
 
     dfs(candidates, target, 0, [], res);
@@ -13,16 +16,24 @@ const combinationSum = (candidates, target) => {
 };
 
 const dfs = (nums, target, index, path, res) => {
+    // 'nums' = to be shared across all rec calls
+    // 'res' = the result container that needs to be shared accross all rec calls
+    // 'target' = new target after using nums[i]
+
     if (target < 0) {
+        // overshot and result not found, terminate the function
         return;
     }
 
     if (target === 0) {
+        // have found the result, add it to the result set
         res.push(path);
+        // then terminate the function
         return;
     }
 
     for (let i = index; i < nums.length; i++) {
+        // path.push will result in LTE
         dfs(nums, target - nums[i], i, path.concat(nums[i]), res);
     }
 };
