8a5fec1624e890a959eb0431f34d66b52757074f
Xi Lu <xilu0317@gmail.com>
2019-09-11 09:21:41 -0700

    new

--- Microsoft/meeting_rooms_1.js
+++ Microsoft/meeting_rooms_1.js
@@ -1,7 +1,18 @@
-/**
- * @param {number[][]} intervals
- * @return {boolean}
- */
+// hint: draw a fking diagram
 const canAttendMeetings = (intervals) => {
+    // Returning true here is per spec
+    if (!intervals || !intervals.length) return true;
 
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    let prev = intervals[0];
+    // Do not use for-of because of the index starts at 1
+    for (let i = 1; i < intervals.length; i++) {
+        cur = intervals[i];
+
+        if (cur[0] < prev[1]) return false;
+        prev = cur;
+    }
+
+    return true;
 };
