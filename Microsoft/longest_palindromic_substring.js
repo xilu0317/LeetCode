6c1d4c8680dff1da2f6457085180b4c449731148
Xi Lu <xilu0317@gmail.com>
2019-09-11 20:03:28 -0700

    new

--- Microsoft/longest_palindromic_substring.js
+++ Microsoft/longest_palindromic_substring.js
@@ -17,6 +17,7 @@ const longestPalindrome = (s) => {
     // grow the palindrome from back to front, try to visualize the movement of indices
     for (let i = n - 1; i >= 0; i--) {
         for (let j = i; j < n; j++) {
+            // j - i <= 2 means at most 3 elements
             if ((s[i] === s[j]) && (j - i <= 2 || dp[i + 1][j - 1])) {
                 dp[i][j] = true;
             }
