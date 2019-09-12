fe6a858729f8f8397d2b1dab1a534dda6a05d1a2
Xi Lu <xilu0317@gmail.com>
2019-09-11 19:08:31 -0700

    new

--- Microsoft/merge_interal.js
+++ Microsoft/merge_interal.js
@@ -1,29 +1,30 @@
-// cur => current interval
-// prev => previous interval
 // hint: order the intervals by their starting time
 
 const merge = (intervals) => {
     if (!intervals || !intervals.length) return [];
 
-    // key step: sort intervals by the left end
+    // KEY
     intervals.sort((a, b) => a[0] - b[0]);
 
     let prev = intervals[0];
-    // put the first interval into 'res'
     let res = [prev];
 
     for (let cur of intervals) {
-        // If the starting position of the current is less than or equal to the ending of the previous
+        // Overlapping between cur start and prev end
         if (cur[0] <= prev[1]) {
-            // then update the previous ending to be whichever to be larger
+            // take whichever is larger and assign to prev
             prev[1] = Math.max(prev[1], cur[1]);
         } else {
-            // If it is a non-touching interval, append it to the result set
+            // If non-touching, add it to the result set
             res.push(cur);
-            // Update the pointer!
+            // don't forget to update the pointer
             prev = cur;
         }
     }
 
     return res;
 };
+
+// Annotation:
+// cur => current interval
+// prev => previous interval
