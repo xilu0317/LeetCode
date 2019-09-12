f83ebd44a4ed779521a7b061cce93ac44c4306fa
Xi Lu <xilu0317@gmail.com>
2019-09-11 18:27:18 -0700

    new

--- Microsoft/number_of_1_bits.js
+++ Microsoft/number_of_1_bits.js
@@ -1,3 +1,5 @@
 const hammingWeight = (n) => {
+    // toString(2) will convert the number to binary representation
+    // [... spread] operator will convert the string into an array
     return [...n.toString(2)].filter(x => x === '1').length;
 };
