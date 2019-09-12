d5cf01a74ec26c168501db072d8c365f622a51d6
Xi Lu <xilu0317@gmail.com>
2019-09-11 22:46:44 -0700

    new

---
+++ Microsoft/spiral_matrix.js
@@ -0,0 +1,39 @@
+const spiralOrder = (matrix) => {
+    if (matrix.length === 0) return [];
+
+    let res = [];
+    let rowBegin = 0, rowEnd = matrix.length - 1;
+    let colBegin = 0, colEnd = matrix[0].length - 1;
+
+    while (rowBegin <= rowEnd && colBegin <= colEnd) {
+        // Traverse Right
+        for (let j = colBegin; j <= colEnd; j++) {
+            res.push(matrix[rowBegin][j]);
+        }
+        rowBegin++;
+
+        // Traverse Down
+        for (let j = rowBegin; j <= rowEnd; j++) {
+            res.push(matrix[j][colEnd]);
+        }
+        colEnd--;
+
+        if (rowBegin <= rowEnd) {
+            // Traverse Left
+            for (let j = colEnd; j >= colBegin; j--) {
+                res.push(matrix[rowEnd][j]);
+            }
+        }
+        rowEnd--;
+
+        if (colBegin <= colEnd) {
+            // Traver Up
+            for (let j = rowEnd; j >= rowBegin; j--) {
+                res.push(matrix[j][colBegin]);
+            }
+        }
+        colBegin++;
+    }
+
+    return res;
+};
