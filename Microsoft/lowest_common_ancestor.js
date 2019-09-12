e2a8a9b6d2fc510a0d93fe49e37aeacc592d526a
Xi Lu <xilu0317@gmail.com>
2019-09-11 20:00:06 -0700

    new

---
+++ Microsoft/lowest_common_ancestor.js
@@ -0,0 +1,24 @@
+/**
+ * @param {TreeNode} cur
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+
+// use 'cur' to reflect dynamic root in nature
+const lowestCommonAncestor = (cur, p, q) => {
+    if (!cur) return null;
+
+    // If the cur node is p or q then the current node is the LCA
+    if (cur === p || cur === q) return cur;
+
+    // recursively check the left and right sub tree
+    let left = lowestCommonAncestor(cur.left, p, q);
+    let right = lowestCommonAncestor(cur.right, p, q);
+
+    // if both are non null then apparently this cur is the LCA
+    if (left && right) return cur;
+
+    // if left is non null and right null then LCA is left. Vice versa.
+    return left ? left : right;
+};
