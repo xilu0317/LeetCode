b59e66c431ed161d0617ff02dee949ddddcfdbe3
Xi Lu <xilu0317@gmail.com>
2019-09-12 22:00:51 -0700

    new

--- Microsoft/roman_to_int.js
+++ Microsoft/roman_to_int.js
@@ -1,27 +1,22 @@
 // Need to explain the observation to the interviewers
 const romanToInt = (s) => {
-    if (!s) return null;
+    let dict = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
 
-    let dict = {};
-    dict['I'] = 1;
-    dict['V'] = 5;
-    dict['X'] = 10;
-    dict['L'] = 50;
-    dict['C'] = 100;
-    dict['D'] = 500;
-    dict['M'] = 1000;
+    let sum = dict[s[s.length - 1]];
 
-    let sum = 0;
-
-    if (s.includes('IV')) sum -= 2;
-    if (s.includes('IX')) sum -= 2;
-    if (s.includes('XL')) sum -= 20;
-    if (s.includes('XC')) sum -= 20;
-    if (s.includes('CD')) sum -= 200;
-    if (s.includes('CM')) sum -= 200;
-
-    for (let i = 0; i < s.length; i++) {
-        sum += dict[s[i]];
+    for (let i = s.length - 2; i >= 0; i--) {
+        if (dict[s[i]] < dict[s[i + 1]])
+            sum -= dict[s[i]];
+        else
+            sum += dict[s[i]];
     }
 
     return sum;
--- java/InsertDeleteRandomO1.java
+++ java/InsertDeleteRandomO1.java
@@ -17,8 +17,10 @@ public class RandomizedSet {
         boolean contain = locs.containsKey(val);
         if (contain)
             return false;
+
         locs.put(val, nums.size());
         nums.add(val);
+
         return true;
     }
 
@@ -30,14 +32,17 @@ public class RandomizedSet {
         boolean contain = locs.containsKey(val);
         if (!contain)
             return false;
+
         int loc = locs.get(val);
         if (loc < nums.size() - 1) { // not the last one than swap the last one with this val
             int lastone = nums.get(nums.size() - 1);
             nums.set(loc, lastone);
             locs.put(lastone, loc);
         }
+
         locs.remove(val);
         nums.remove(nums.size() - 1);
+
         return true;
     }
 
