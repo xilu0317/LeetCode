8fde2dfda3c43debbf7ff05f70cd75a8b8b639c7
Xi Lu <xilu0317@gmail.com>
2019-09-13 09:56:52 -0700

    new

--- Microsoft/stack_using_queues.js
+++ Microsoft/stack_using_queues.js
@@ -1,43 +1,33 @@
-/**
- * Initialize your data structure here.
- */
 class MyStack {
     constructor() {
         this.q1 = [];
         this.q2 = [];
     }
-    /**
-     * Push element x onto stack.
-     * @param {number} x
-     * @return {void}
-     */
+
     push(x) {
         this.q1.push(x);
     }
-    /**
-     * Removes the element on top of the stack and returns that element.
-     * @return {number}
-     */
+
     pop() {
+        // lock length
         let len = this.q1.length;
+
         for (let i = 0; i < len - 1; i++) {
             this.q2.push(this.q1.shift());
         }
+
         let result = this.q1.shift();
+
+        // ES6
         [this.q1, this.q2] = [this.q2, this.q1];
+
         return result;
     }
-    /**
-     * Get the top element.
-     * @return {number}
-     */
+
     top() {
         return this.q1[this.q1.length - 1];
     }
-    /**
-     * Returns whether the stack is empty.
-     * @return {boolean}
-     */
+
     empty() {
         return this.q1.length === 0;
     }
