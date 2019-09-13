d361d3ea6498e5d78b92c7d577607efd227931a1
Xi Lu <xilu0317@gmail.com>
2019-09-12 22:09:23 -0700

    new

--- Microsoft/roman_to_int.js
+++ Microsoft/roman_to_int.js
@@ -23,3 +23,30 @@ const romanToInt = (s) => {
 
     return sum;
 };
+
+// EASY TO UNDERSTAND
+const romanToInt = function (s) {
+    let map = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    let sum = 0;
+    for (let i = 0; i < s.length; ++i)
+        sum += map[s[i]];
+
+    // rectify the values
+    if (s.includes('IV')) sum -= 2;
+    if (s.includes('IX')) sum -= 2;
+    if (s.includes('XL')) sum -= 20;
+    if (s.includes('XC')) sum -= 20;
+    if (s.includes('CD')) sum -= 200;
+    if (s.includes('CM')) sum -= 200;
+
+    return sum;
+};
