80268c22269f1904ee5902c60e95f20e6daf2050
Xi Lu <xilu0317@gmail.com>
2019-09-13 10:07:09 -0700

    new

--- Microsoft/queue_using_stacks.js
+++ Microsoft/queue_using_stacks.js
@@ -8,11 +8,7 @@ class MyQueue {
         this.s2 = [];
         this.s2Copy = [];
     }
-    /**
-     * Push element x to the back of queue.
-     * @param {number} x
-     * @return {void}
-     */
+
     push(x) {
         this.s1.push(x);
         this.s2.length = 0;
@@ -21,10 +17,7 @@ class MyQueue {
             this.s2.push(this.s1Copy.pop());
         }
     }
-    /**
-     * Removes the element from in front of queue and returns that element.
-     * @return {number}
-     */
+
     pop() {
         let result = this.s2.pop();
         this.s1.length = 0;
@@ -34,17 +27,11 @@ class MyQueue {
         }
         return result;
     }
-    /**
-     * Get the front element.
-     * @return {number}
-     */
+
     peek() {
         return this.s1[0];
     }
-    /**
-     * Returns whether the queue is empty.
-     * @return {boolean}
-     */
+
     empty() {
         return this.s1.length === 0;
     }
