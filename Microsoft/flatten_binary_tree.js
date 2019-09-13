75ee61229e8fb2c53af1310241a23fcaff859828
Xi Lu <xilu0317@gmail.com>
2019-09-12 17:20:06 -0700

    new

--- Microsoft/flatten_binary_tree.js
+++ Microsoft/flatten_binary_tree.js
@@ -1,67 +1,21 @@
-const stuff = require('../basics/bst.js');
+// Best recursive solution
+const flatten = (root) => {
+    if (!root) return;
 
-class TreeNode {
-    constructor(val) {
-        this.val = val;
-        this.left = this.right = null;
-    }
-}
+    // record left and right
+    let oldLeft = root.left;
+    let oldRight = root.right;
 
-/**
- * @param {TreeNode} root
- * @return {void} Do not return anything, modify root in-place instead.
- */
+    // as per spec set left to zero
+    root.left = null;
 
-// my own [BAD SOLUTION]
-function _flatten(cur) {
-    if (!cur) return null;
+    flatten(oldLeft);
+    flatten(oldRight);
 
-    if (!cur.left && !cur.right) return cur;
+    root.right = oldLeft;
 
-    if (!cur.left) {
-        return _flatten(cur.right);
-    }
-
-    if (!cur.right) {
-        cur.right = cur.left;
-        cur.left = null;
-        return _flatten(cur.right);
-    }
-
-    let leftTreeTail = _flatten(cur.left);
-    let rightTreeTail = _flatten(cur.right);
-
-    let tmpRightHead = cur.right;
-    cur.right = cur.left;
-    cur.left = null;
-
-    leftTreeTail.left = null;
-    leftTreeTail.right = tmpRightHead;
-
-    return rightTreeTail;
-}
-
-const flatten_xi = (root) => {
-    _flatten(root);
-};
-
-let root = stuff([1, 2]);
-flatten_xi(root);
-
-// Pre-Order TODO: revisit
-// https://longwayjade.wordpress.com/2015/04/23/leetcode-recursion-flatten-binary-tree-to-linked-list/
-let prev = null;
-const flatten = (cur) => {
-    if (!cur) return;
-
-    let temp = cur.right;
-
-    if (prev) {
-        prev.left = null;
-        prev.right = cur;
-    }
-
-    prev = cur;
-    flatten(cur.left);
-    flatten(temp);
+    let cur = root;
+    while (cur.right)
+        cur = cur.right;
+    cur.right = oldRight;
 };
