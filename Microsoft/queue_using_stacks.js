14772ad3ec8b7f4dfc45d47dfccc1ca617d6c984
Xi Lu <xilu0317@gmail.com>
2019-09-13 10:09:12 -0700

    new

--- Microsoft/queue_using_stacks.js
+++ Microsoft/queue_using_stacks.js
@@ -1,6 +1,3 @@
-/**
- * Initialize your data structure here.
- */
 class MyQueue {
     constructor() {
         this.s1 = [];
@@ -13,6 +10,7 @@ class MyQueue {
         this.s1.push(x);
         this.s2.length = 0;
         this.s1Copy = Object.assign([], this.s1);
+
         while (this.s1Copy.length > 0) {
             this.s2.push(this.s1Copy.pop());
         }
@@ -20,11 +18,14 @@ class MyQueue {
 
     pop() {
         let result = this.s2.pop();
+
         this.s1.length = 0;
         this.s2Copy = Object.assign([], this.s2);
+
         while (this.s2Copy.length > 0) {
             this.s1.push(this.s2Copy.pop());
         }
+
         return result;
     }
 
