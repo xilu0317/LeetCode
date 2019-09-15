8bb2b4a9750af7001b75032d1ae7a90086b86caf
Xi Lu <xilu0317@gmail.com>
2019-09-14 17:14:09 -0700

    new

---
+++ leetcode/regex_matching.js
@@ -0,0 +1,33 @@
+const isMatch = (s, p) => {
+    if (s === null || p === null) return false;
+
+    let sLen = s.length, pLen = p.length;
+    let dp = Array(sLen + 1)
+                        .fill()
+                        .map(() => Array(pLen + 1).fill(false));
+
+    dp[0][0] = true;
+    for (let i = 0; i < pLen; i++) {
+        if (p[i] === '*' && dp[0][i - 1])
+            dp[0][i + 1] = true;
+    }
+
+    for (let i = 0; i < sLen; i++) {
+        for (let j = 0; j < pLen; j++) {
+            if (p[j] === '.')
+                dp[i + 1][j + 1] = dp[i][j];
+
+            if (p[j] === s[i])
+                dp[i + 1][j + 1] = dp[i][j];
+
+            if (p[j] === '*') {
+                if (p[j - 1] !== s[i] && p[j - 1] !== '.')
+                    dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                else
+                    dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+            }
+        }
+    }
+
+    return dp[sLen][pLen];
+};
