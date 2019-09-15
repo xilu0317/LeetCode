d4fdda280feb1297961f173b52397776f879d9ff
Xi Lu <xilu0317@gmail.com>
2019-09-15 07:59:09 -0700

    new

--- Microsoft/path_sum_1.js
+++ Microsoft/path_sum_1.js
@@ -1,25 +1,24 @@
 const hasPathSum = (root, sum) => {
     if (!root) return [];
 
+    // KEY maintain a dynamic total
     let stack = [[root, 0]];
 
+    // iterative dfs
     while (stack.length) {
-        // dfs always backtracks to the last tracked node
+        // backtract to the previous step
         let [node, total] = stack.pop();
 
-        // if the cur node is a leaf
-        if (!node.left && !node.right) {
+        // if a leaf
+        if (!node.left && !node.right)
             if (total + node.val === sum) return true;
-        }
 
         // the usual adding children bit, the current nodal value has been added into the total
-        if (node.right) {
+        if (node.right)
             stack.push([node.right, total + node.val]);
-        }
 
-        if (node.left) {
+        if (node.left)
             stack.push([node.left, total + node.val]);
-        }
     }
 
     return false;
