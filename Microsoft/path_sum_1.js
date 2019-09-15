bd7c67d9fe30c54271e7a94005c0c84bbdc5c0c6
Xi Lu <xilu0317@gmail.com>
2019-09-15 16:02:30 -0700

    new

--- Microsoft/path_sum_1.js
+++ Microsoft/path_sum_1.js
@@ -24,9 +24,10 @@ const hasPathSum = (root, sum) => {
     return false;
 };
 
-// TODO: recursion
-// bool hasPathSum(TreeNode * root, int sum) {
-//     if (root == NULL) return false;
-//     if (root -> val == sum && root -> left == NULL && root -> right == NULL) return true;
-//     return hasPathSum(root -> left, sum - root -> val) || hasPathSum(root -> right, sum - root -> val);
-// }
+const hasPathSum = (root, sum) => {
+    if (!root) return false;
+
+    if (!root.left && !root.right && root.val === sum) return true;
+
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+};
