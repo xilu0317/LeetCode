e44f27b9bd52f61cc0cd17ad53f03f01c31a24db
Xi Lu <xilu0317@gmail.com>
2019-09-14 17:24:28 -0700

    new

--- leetcode/regex_matching.js
+++ leetcode/regex_matching.js
@@ -3,9 +3,10 @@ const isMatch = (s, p) => {
 
     let sLen = s.length, pLen = p.length;
     let dp = Array(sLen + 1)
-                        .fill()
-                        .map(() => Array(pLen + 1).fill(false));
+        .fill()
+        .map(() => Array(pLen + 1).fill(false));
 
+    // initialization
     dp[0][0] = true;
     for (let i = 0; i < pLen; i++) {
         if (p[i] === '*' && dp[0][i - 1])
@@ -14,12 +15,15 @@ const isMatch = (s, p) => {
 
     for (let i = 0; i < sLen; i++) {
         for (let j = 0; j < pLen; j++) {
+            // if the pattern is a '.', inherit the previous truth value
             if (p[j] === '.')
                 dp[i + 1][j + 1] = dp[i][j];
 
+            // if the current char is an exact equal, inherit the previous truth value
             if (p[j] === s[i])
                 dp[i + 1][j + 1] = dp[i][j];
 
+            //
             if (p[j] === '*') {
                 if (p[j - 1] !== s[i] && p[j - 1] !== '.')
                     dp[i + 1][j + 1] = dp[i + 1][j - 1];
