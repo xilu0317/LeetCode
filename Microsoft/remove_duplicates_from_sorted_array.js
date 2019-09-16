3154edbd39e233734ee15bbcec2d8dfba930371a
Xi Lu <xilu0317@gmail.com>
2019-09-15 23:32:06 -0700

    new

--- Microsoft/remove_duplicates_from_sorted_array.js
+++ Microsoft/remove_duplicates_from_sorted_array.js
@@ -1,16 +1,16 @@
+// TODO: revisit
 const removeDuplicates = (nums) => {
     if (!nums) return -1;
 
-    let slow = 0;
-    let fast = 1;
+    let slow = 0, fast = 1;
 
     while (fast < nums.length) {
         if (nums[fast] === nums[fast - 1]) {
-            ++fast;
+            fast++;
         } else {
             nums[slow + 1] = nums[fast];
-            ++slow;
-            ++fast;
+            slow++;
+            fast++;
         }
     }
 
