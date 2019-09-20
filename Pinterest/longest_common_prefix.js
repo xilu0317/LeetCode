969a1ca625a76747c641ecad9a5631e22a4254b8
Xi Lu <xilu0317@gmail.com>
2019-09-20 00:08:56 -0700

    new

---
+++ Pinterest/longest_common_prefix.js
@@ -0,0 +1,17 @@
+const longestCommonPrefix = (strs) => {
+    if (!strs || !strs.length) return '';
+
+    let minLen = Math.min(...strs.map(x => x.length));
+
+    let res = '';
+    for (let i = 0; i < minLen; i++) {
+        let common = strs[0][i];
+
+        if (strs.map(x => x[i]).every(x => x === common))
+            res += common;
+        else
+            break;
+    }
+
+    return res;
+};
