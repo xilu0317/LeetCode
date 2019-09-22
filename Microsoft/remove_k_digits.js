74d4e9417c09110f8153b8a34b62c433b5f3a8d8
Xi Lu <xilu0317@gmail.com>
2019-09-21 22:26:17 -0700

    new

---
+++ Microsoft/remove_k_digits.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+const removeKdigits = (num, k) => {
+    // num += '';
+
+    // let stack = [];
+    // let res = '';
+    // for (let i = 0; i < num.length; i++) {
+    //     let cur = num[i];
+
+    //     stack.push(cur);
+
+    //     if (stack[stack.length - 1] > cur && k > 0) {
+    //         stack.pop();
+    //         k--;
+    //     }
+
+    //     res += cur;
+    // }
+
+    // return res;
+};
