3f563440a10a2c7fc1472005e0d26ef4e20aa510
Xi Lu <xilu0317@gmail.com>
2019-09-22 12:06:30 -0700

    new

--- Amazon/two_sum_closest.js
+++ Amazon/two_sum_closest.js
@@ -1,8 +1,11 @@
 const twoSumClosest = (nums, target) => {
+    // KEY: sorting
     nums.sort((a, b) => a > b);
+
     let min = Infinity;
     let left = 0, right = nums.length - 1;
 
+    // KEY: Try an example 3 5 7 8 8 9 12 15
     while (left < right) {
         if (nums[left] + nums[right] < target) {
             min = Math.min(min, target - nums[left] - nums[right]);
