c20dff9ec0fad35a5a35253e8abd0756eeed3bc5
Xi Lu <xilu0317@gmail.com>
2019-09-22 19:44:37 -0700

    new

--- Amazon/reorder_log_files.js
+++ Amazon/reorder_log_files.js
@@ -1,20 +1,22 @@
-// KEY: come up with a good comparator
+// KEY: come up with a good comp
 const comp = (a, b) => {
-    let aRest = a.substring(a.indexOf(' ') + 1);
-    let bRest = b.substring(b.indexOf(' ') + 1);
+    let aIndex = a.indexOf(' '), bIndex = b.indexOf(' ');
+    // look for the first space
+    let aRest = a.substring(aIndex + 1);
+    let bRest = b.substring(bIndex + 1);
 
     if (aRest > bRest)
         return 1;
-    else if (aRest < bRest)
+    if (aRest < bRest)
         return -1;
 
-    let aBefore = a.substring(0, a.indexOf('_') + 1);
-    let bBefore = b.substring(0, b.indexOf('_') + 1);
+    let aBefore = a.substring(0, aIndex + 1);
+    let bBefore = b.substring(0, bIndex + 1);
 
     if (aBefore > bBefore)
-        return -1;
-    else
         return 1;
+    else
+        return -1;
 };
 
 const reorderLogFiles = (logs) => {
