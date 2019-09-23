747c78d42d0e0470a43720b8a56298234b19934e
Xi Lu <xilu0317@gmail.com>
2019-09-22 17:51:35 -0700

    new

---
+++ Amazon/treasure_island.js
@@ -0,0 +1,48 @@
+(function main() {
+    const grid = [['O', 'O', 'O', 'O'],
+    ['D', 'O', 'D', 'O'],
+    ['O', 'O', 'O', 'O'],
+    ['X', 'D', 'D', 'O']];
+
+    const isSafe = (x, y, width, height) => { return x >= 0 && x <= width && y >= 0 && y <= height && grid[x][y] !== 'D'; };
+    const [dx, dy] = [[0, 1, 0, -1], [1, 0, -1, 0]];
+    const [startX, startY, endX, endY] = [0, 0, grid[0].length - 1, grid.length - 1];
+
+    const minStep = () => {
+        let node = {
+            x: startX,
+            y: startY,
+            val: 'O',
+            step: 0,
+        };
+
+        let q = [node];
+        while (q.length) {
+            let cur = q.shift();
+
+            if (cur.val === 'X') {
+                return cur.step;
+            }
+
+            for (let i = 0; i < dx.length; i++) {
+                let [nextX, nextY] = [cur.x + dx[i], cur.y + dy[i]];
+                
+                if (!isSafe(nextX, nextY, endX, endY)) continue;
+                let next = {
+                    x: nextX,
+                    y: nextY,
+                    val: grid[nextX][nextY],
+                    step: cur.step + 1,
+                }
+                
+                grid[nextX][nextY] = 'D';
+                q.push(next);
+            }
+        }
+        return -1;
+    };
+
+    let min = minStep();
+    console.log(min);
+    return min;
+}());
