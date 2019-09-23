76a27815d2d791232bb2f8bbcdd4b795835a5d05
Xi Lu <xilu0317@gmail.com>
2019-09-22 22:24:02 -0700

    new

--- PriorityQueue/MinHeap.js
+++ PriorityQueue/MinHeap.js
@@ -1,39 +1,39 @@
 // Implement inter based min heap
 class MinHeap {
 
-	constructor() {
-		this.arr = [];
+    constructor() {
+        this.arr = [];
 
-	}
+    }
 
-	dump() {
-		return this.arr;
-	}
+    dump() {
+        return this.arr;
+    }
 
-	push(x) {
-		this.arr.push(x);
-		this.bubbleUp(this.arr.length - 1);
-	}
+    push(x) {
+        this.arr.push(x);
+        this.bubbleUp(this.arr.length - 1);
+    }
 
-	shift() {
-		let val = this.arr[0];
-	}
+    shift() {
+        let val = this.arr[0];
+    }
 
-	bubbleUp(i) {
+    bubbleUp(i) {
 
-	}
+    }
 
-	bubbleDown(i) {
+    bubbleDown(i) {
 
-	}
+    }
 
-	getParentIndex(i) {
+    getParentIndex(i) {
 
-	}
+    }
 
-	getChildIndex(i) {
+    getChildIndex(i) {
 
-	}
+    }
 }
 
 
