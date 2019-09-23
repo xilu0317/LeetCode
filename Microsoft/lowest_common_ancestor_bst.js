5ed6ce5bde45275bdd76c64c7a49b6c2768972d5
Xi Lu <xilu0317@gmail.com>
2019-09-22 23:26:35 -0700

    new

---
+++ Microsoft/lowest_common_ancestor_bst.js
@@ -0,0 +1,10 @@
+const lowestCommonAncestor = (root, p, q) => {
+    if (root.val > p.val && root.val > q.val)
+        return lowestCommonAncestor(root.left, p, q);
+
+    if (root.val < p.val && root.val < q.val)
+        return lowestCommonAncestor(root.right, p, q);
+
+    // note this does take care !root edge case and more elegant
+    return root;
+};
