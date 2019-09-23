0d8ab26dfb8f4aeda6011c8ba452b2602c67d84f
Xi Lu <xilu0317@gmail.com>
2019-09-22 18:32:03 -0700

    new

--- Amazon/treasure_island.js
+++ Amazon/treasure_island.js
@@ -1,11 +1,16 @@
 (function main() {
     const grid = [['O', 'O', 'O', 'O'],
-    ['D', 'O', 'D', 'O'],
-    ['O', 'O', 'O', 'O'],
-    ['X', 'D', 'D', 'O']];
+                 ['D', 'O', 'D', 'O'],
+                 ['O', 'O', 'O', 'O'],
+                 ['X', 'D', 'D', 'O']];
 
-    const isSafe = (x, y, width, height) => { return x >= 0 && x <= width && y >= 0 && y <= height && grid[x][y] !== 'D'; };
+    const isSafe = (x, y, width, height) => {
+        return x >= 0 && x <= width && y >= 0 && y <= height && grid[x][y] !== 'D';
+    };
+    // unit vector, destructuring
     const [dx, dy] = [[0, 1, 0, -1], [1, 0, -1, 0]];
+
+    // destructring
     const [startX, startY, endX, endY] = [0, 0, grid[0].length - 1, grid.length - 1];
 
     const minStep = () => {
@@ -20,29 +25,33 @@
         while (q.length) {
             let cur = q.shift();
 
+            // got treasure
             if (cur.val === 'X') {
                 return cur.step;
             }
 
             for (let i = 0; i < dx.length; i++) {
                 let [nextX, nextY] = [cur.x + dx[i], cur.y + dy[i]];
-                
-                if (!isSafe(nextX, nextY, endX, endY)) continue;
+
+                if (!isSafe(nextX, nextY, endX, endY))
+                    continue;
+
                 let next = {
                     x: nextX,
                     y: nextY,
                     val: grid[nextX][nextY],
                     step: cur.step + 1,
                 }
-                
+
+                // mark as visisted
                 grid[nextX][nextY] = 'D';
                 q.push(next);
             }
         }
+
         return -1;
     };
 
-    let min = minStep();
-    console.log(min);
-    return min;
+    // test the code
+    console.log('min step => ' + minStep());
 }());
