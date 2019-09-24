db788a5f033e77d39853fc456b641a454dcf077f
Xi Lu <xilu0317@gmail.com>
2019-09-23 19:48:34 -0700

    new

--- Microsoft/integer_to_english.js
+++ Microsoft/integer_to_english.js
@@ -1,3 +1,4 @@
+// TODO: revisit
 let LESS_THAN_20 = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
 
 let TENS = ['', 'Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
