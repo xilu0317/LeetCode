8121d62b8cb2afa8c8b938406d97a8a46307bb9d
Xi Lu <xilu0317@gmail.com>
2019-10-06 16:55:22 -0700

    new

--- Amazon/optimal_utilization.js
+++ Amazon/optimal_utilization.js
@@ -1,15 +1,14 @@
 // https://leetcode.com/discuss/interview-question/373202/Amazon-or-OA-2019-or-Optimal-Utilization
 
-// TODO: REVISIT still some bugs here
 (() => {
 
-    a = [[1, 8], [2, 7], [3, 14]]
-    b = [[1, 5], [2, 10], [3, 14]]
-    target = 20
+    // a = [[1, 8], [2, 7], [3, 14]]
+    // b = [[1, 5], [2, 10], [3, 14]]
+    // target = 20
 
-    // a = [[1, 3], [2, 5], [3, 7], [4, 10]];
-    // b = [[1, 2], [2, 3], [3, 4], [4, 5]];
-    // target = 10;
+    a = [[1, 3], [2, 5], [3, 7], [4, 10]];
+    b = [[1, 2], [2, 3], [3, 4], [4, 5]];
+    target = 10;
 
     const twoSumClosest = (a, b, target) => {
         let i = 0; j = b.length - 1;
@@ -17,10 +16,9 @@
 
         // just identify the min in the first pass
         while (i < a.length && j >= 0) {
-            if (target - a[i][1] - b[j][1] < 0)
-                break;
-
-            min = Math.min(min, target - a[i][1] - b[j][1]);
+            let diff = target - a[i][1] - b[j][1];
+            if (diff >= 0)
+                min = Math.min(min, diff);
 
             if (a[i][1] + b[j][1] > target)
                 j--;
@@ -46,8 +44,6 @@
         return res;
     };
 
-    console.log('fuck it ');
     let stuff = twoSumClosest(a, b, target);
-    console.log('solution = ', stuff);
-    console.log('done');
+    console.log(stuff.toString());
 })();
