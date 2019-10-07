5629ba0057a208edb8f48708b411ec9576d1806b
Xi Lu <xi.lu@sophos.com>
2019-10-07 15:59:22 -0700

    new

--- basics/trie.js
+++ basics/trie.js
@@ -1,28 +1,27 @@
 // ES6 class
 class Trie {
     constructor() {
-        // instance variable
-        // '{}' same as an new object
+        // This is how you define memeber vars in ES6
         this.root = {};
     }
 
     // The essence is to build the prefix tree using the insert method
     insert(word) {
         let cur = this.root;
-        for (const c of word) {
+        for (let c of word) {
             // if cur[c] exists use current, otherwise create an new object
             cur[c] = cur[c] || {};
             // update the 'cur' pointer
             cur = cur[c];
         }
         // Make sure to flag the last node
-        cur.isWord = true;
+        cur.isLastNode = true;
     }
 
     // This method will traverse all the way till the last node
     traverse(word) {
         let cur = this.root;
-        for (const c of word) {
+        for (let c of word) {
             if (!cur) {
                 return null;
             }
@@ -35,10 +34,11 @@ class Trie {
 
     search(word) {
         // Get the last node
-        let lastNode = this.traverse(word);
+        let node = this.traverse(word);
 
-        // '!!' = converts everything else to bool
-        return !!lastNode && !!lastNode.isWord;
+        // '!!' will turn truthy to true
+        // If the last node exists and it is the last word
+        return !!node && !!node.isLastNode;
     }
 
     startsWith(word) {
@@ -55,7 +55,7 @@ function Trie() {
     function insert(word) {
         let cur = root;
         word.split('').forEach(c => cur = cur[c] = (cur[c] || {}));
-        cur.isWord = true;
+        cur.isLastNode = true;
     }
 
     function traverse(word) {
@@ -70,9 +70,9 @@ function Trie() {
     }
 
     function search(word) {
-        let lastNode = traverse(word);
+        let node = traverse(word);
 
-        return !!lastNode && !!lastNode.isWord;
+        return !!node && !!node.isLastNode;
     }
 
     function startsWith(word) {
@@ -96,4 +96,3 @@ Trie.prototype.search = function (word) {
 Trie.prototype.startsWith = function (prefix) {
 
 };
-
