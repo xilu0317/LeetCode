d72637046aad26b78b87cc28ee931b2b9d7a2a1a
Xi Lu <xilu0317@gmail.com>
2019-10-09 23:26:42 -0700

    new

--- Indeed/summary_ranges.js
+++ Indeed/summary_ranges.js
@@ -3,6 +3,7 @@ const summaryRanges = (nums) => {
 
     for (let i = 0; i < nums.length; i++) {
         let prev = nums[i];
+        // keep increasing the index as long as the difference between cur and next is 1
         while (i + 1 < nums.length && nums[i + 1] - nums[i] === 1)
             i++;
 
