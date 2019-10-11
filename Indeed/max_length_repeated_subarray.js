3793c63a31227f11548c7667844c98883389a538
Xi Lu <xi.lu@sophos.com>
2019-10-11 11:26:59 -0700

    new

--- Indeed/max_length_repeated_subarray.js
+++ Indeed/max_length_repeated_subarray.js
@@ -1,4 +1,19 @@
-// https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+const findLength = (A, B) => {
+    if (!A || !B || !A.length || !B.length) return 0;
 
+    let m = A.length, n = B.length;
+    let max = 0;
+    let dp = Array(m + 1).fill()
+                         .map(() => Array(n + 1).fill(0));
 
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (A[i - 1] === B[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+    }
 
+    return max;
+};
