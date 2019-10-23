64bdb7717314fa08e0b1ec2d2e3f05a0783b664a
Xi Lu <xilu0317@gmail.com>
2019-10-22 22:17:20 -0700

    new

--- Indeed/robot_origin.js
+++ Indeed/robot_origin.js
@@ -1,7 +1,24 @@
-/**
- * @param {string} moves
- * @return {boolean}
- */
 const judgeCircle = (moves) => {
+    if (!moves) return false;
 
+    let x = 0, y = 0;
+
+    for (let move of moves) {
+        switch (move) {
+            case 'L':
+                x--;
+                break;
+            case 'R':
+                x++;
+                break;
+            case 'U':
+                y++;
+                break;
+            case 'D':
+                y--;
+                break;
+        }
+    }
+
+    return x === 0 && y === 0;
 };
