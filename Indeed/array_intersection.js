7854ae0caa0c2520aa0ab2151efa0965c46bbcae
Xi Lu <xilu0317@gmail.com>
2019-10-22 22:00:31 -0700

    new

---
+++ Indeed/array_intersection.js
@@ -0,0 +1,12 @@
+const intersection = (nums1, nums2) => {
+    if (!nums1 || !nums2) return [];
+
+    let s = new Set(nums1);
+    let res = [];
+    for (let item of nums2) {
+        if (s.has(item))
+            res.push(item);
+    }
+
+    return [...new Set(res)];
+};
