9b9bd91ced9483d98c76ebddff442c671aaa77c0
Xi Lu <xi.lu@sophos.com>
2019-10-23 17:24:06 -0700

    new

---
+++ Indeed/serialize_binary_tree.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+const serialize = (root) => {
+    
+};
+
+const serializeHelper = (root) => {
+
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+const deserialize = (data) => {
+    
+};
+
+const deserializeHelper = (root) => {
+
+};
