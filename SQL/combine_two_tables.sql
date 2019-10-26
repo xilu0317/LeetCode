f63d4c80dc21faffc32d37ff87e1348539f23acc
Xi Lu <xilu0317@gmail.com>
2019-10-26 15:50:00 -0700

    new

--- SQL/combine_two_tables.sql
+++ SQL/combine_two_tables.sql
@@ -19,11 +19,12 @@
 -- | State       | varchar |
 -- +-------------+---------+
 -- AddressId is the primary key column for this table.
- 
 
 -- Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
 
 -- FirstName, LastName, City, State
 
-SELECT Person.FirstName, Person.LastName, Address.City, Address.State 
-FROM Person LEFT JOIN Address ON Person.PersonId = Address.PersonId;
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State
+FROM Person
+LEFT JOIN Address
+ON Person.PersonId = Address.PersonId;
