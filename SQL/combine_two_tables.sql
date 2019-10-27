4cc74cfe244b7f84f0c966e369ffa310e590a0d9
Xi Lu <xilu0317@gmail.com>
2019-10-27 12:03:17 -0700

    new

--- SQL/combine_two_tables.sql
+++ SQL/combine_two_tables.sql
@@ -19,7 +19,8 @@
 -- +-------------+---------+
 -- AddressId is the primary key column for this table.
 
--- Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+-- Write a SQL query for a report that provides the following information for each person in the Person table, 
+-- regardless if there is an address for each of those people:
 -- FirstName, LastName, City, State
 
 SELECT Person.FirstName, 
