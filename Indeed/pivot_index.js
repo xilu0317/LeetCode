041d0a8421e8bff49ce304db9f4c1b724017ce15
Xi Lu <xilu0317@gmail.com>
2019-10-26 20:54:33 -0700

    new

---
+++ Indeed/pivot_index.js
@@ -0,0 +1,19 @@
+const pivotIndex = (nums) => {
+    if (!nums || !nums.length) return -1;
+
+    let right = nums.reduce((acc, cur) => acc + cur);
+    let left = 0;
+    let index = 0;
+    for (let num of nums) {
+        right -= num;
+
+        if (left === right)
+            return index;
+
+        left += num;
+
+        index++;
+    }
+
+    return -1;
+};
