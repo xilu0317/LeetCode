006e425e773d510a8b416a2d72c57502b445f941
Xi Lu <xilu0317@gmail.com>
2019-10-26 21:42:48 -0700

    new

--- Indeed/subarray_sum_equal_k.js
+++ Indeed/subarray_sum_equal_k.js
@@ -1,21 +1,14 @@
-// TODO: need to rework
-
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
 const subarraySum = (nums, k) => {
     let sum = 0, res = 0;
     let map = new Map();
+    map.set(0, 1);
 
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
-        if (map.has(sum - k)) {
+        if (map.has(sum - k))
             res += map.get(sum - k);
-        }
 
-        map.set(sum, map.get(sum) + 1 || + 1);
+        map.set(sum, (map.get(sum) || 0) + 1);
     }
 
     return res;
