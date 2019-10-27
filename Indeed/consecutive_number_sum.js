3893ceb025edc8ce88901c940638ad4a0a0f0532
Xi Lu <xilu0317@gmail.com>
2019-10-27 11:18:57 -0700

    new

--- Indeed/consecutive_number_sum.js
+++ Indeed/consecutive_number_sum.js
@@ -1,14 +1,16 @@
-// please derive the general term formula for the algebric sequence
+// KEY: derive the general term formula for the algebric sequence
 
 const consecutiveNumbersSum = (N) => {
     let res = 0;
     // didn't specify the running condition because of the 'break'
     for (let m = 1; ; m++) {
         let xm = N - m * (m - 1) / 2; // need to derive this
-        // consecutive positive integers AND numbers of terms can never be zero
+        // consecutive positive integers 
+        // m (numbers of terms) can never be zero
         if (xm <= 0)
             break;
-        // check if xm divided by m is still an integer
+
+        // check if x (the initial term of the sequence is an integer)
         if (xm % m === 0)
             res++;
     }
