fe11eb79209dd310c0247272609506d46c875df1
Xi Lu <xi.lu@sophos.com>
2019-11-04 11:04:30 -0800

    new

--- Indeed/pivot_index.js
+++ Indeed/pivot_index.js
@@ -1,13 +1,13 @@
 const pivotIndex = (nums) => {
     if (!nums || !nums.length) return -1
 
-    let right = nums.reduce((acc, cur) => acc + cur)
-    let left = 0, index = 0
+    let left = 0, right = nums.reduce((acc, cur) => acc + cur)
+
+    let index = 0
     for (let num of nums) {
         right -= num
 
-        if (left === right)
-            return index
+        if (left === right) return index
 
         left += num
 
