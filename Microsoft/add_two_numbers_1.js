c2dc5c7e7ed65a9b16307bacc35e88bab5cd860e
Xi Lu <xilu0317@gmail.com>
2019-11-03 22:48:16 -0800

    new

--- Microsoft/add_two_numbers_1.js
+++ Microsoft/add_two_numbers_1.js
@@ -1,28 +1,28 @@
 const addTwoNumbers = (l1, l2) => {
-    let cur1 = l1, cur2 = l2;
-    let cur = dummy = new ListNode(0);
-    let sum = 0;
+    let cur1 = l1, cur2 = l2
+    let cur = dummy = new ListNode(0)
+    let sum = 0
 
     while (cur1 || cur2) {
         // KEY carry tenth place's digit over
-        sum = parseInt(sum / 10);
+        sum = parseInt(sum / 10)
 
         if (cur1) {
-            sum += cur1.val;
-            cur1 = cur1.next;
+            sum += cur1.val
+            cur1 = cur1.next
         }
 
         if (cur2) {
-            sum += cur2.val;
-            cur2 = cur2.next;
+            sum += cur2.val
+            cur2 = cur2.next
         }
 
-        cur.next = new ListNode(sum % 10);
-        cur = cur.next;
+        cur.next = new ListNode(sum % 10)
+        cur = cur.next
     }
 
     // account for the last '1'
-    if (sum >= 10) cur.next = new ListNode(1);
+    if (sum >= 10) cur.next = new ListNode(1)
 
-    return dummy.next;
-};
+    return dummy.next
+}
