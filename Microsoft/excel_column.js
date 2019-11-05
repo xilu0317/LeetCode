38e3ed03578083e045c2aa7ffade4bc4e76ec6b0
Xi Lu <xilu0317@gmail.com>
2019-11-05 09:12:08 -0800

    new

--- Microsoft/excel_column.js
+++ Microsoft/excel_column.js
@@ -2,13 +2,15 @@
 const convertToTitle = (n) => {
     let len = 26, dict = {}, res = []
 
+    // map numberal to char
     for (let i = 0; i < len; i++) {
-        // map numberal to char
         dict[i] = String.fromCharCode(i + 65)
     }
 
     while (n) {
+        // calculate the 26 based number using moduale operator
         res.unshift(dict[(n - 1) % len])
+        // calculate the '10th' place value
         n = parseInt((n - 1) / len)
     }
 
