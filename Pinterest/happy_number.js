0a6220072cf209429ea0a9ee08d545b7189480bc
U-fire-PC\fire <xilu.fullstack@gmail.com>
2019-11-06 21:47:28 -0800

    new

--- Pinterest/happy_number.js
+++ Pinterest/happy_number.js
@@ -1,15 +1,17 @@
+// helper function used to do the calculation
 const calculate = (n) => {
     let sum = 0
 
     while (n) {
-        let num = n % 10
+        d = n % 10
         n = parseInt(n / 10)
-        sum += num ** 2
+        sum += d ** 2
     }
 
     return sum
 }
 
+// recall slow and faster pointer to detect cycle in a linkedlist
 const isHappy = (n) => {
     let slow = fast = n
     do {
