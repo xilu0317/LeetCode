cd99b5a4e22de430137a95b2f384215993957156
Xi Lu <xilu0317@gmail.com>
2019-11-12 11:43:00 -0800

    new

--- FE_engineering/es6_class.js
+++ FE_engineering/es6_class.js
@@ -1,26 +1,21 @@
 class Student {
-    // static variable
+    // class/static variable
     static count = 0
 
     constructor(name, age, gpa) {
         // instance variables
         this.name = name
-        this.age = age
-        this.gpa = gpa
 
+        // accessing class/static method
         Student.count++
     }
 
-    // instance methods
+    // instance method
     getName() {
-        console.log('Name = ', this.name)
+        console.log(`name = ${this.name}`)
     }
 
-    getAge() {
-        console.log('Age = ', this.age)
-    }
-
-    // static methods
+    // class/static method
     static showTotalNumberOfStudents() {
         console.log('The total number of students is ', Student.count)
     }
@@ -30,15 +25,10 @@ let s1 = new Student('Alex', 17, 3.12)
 let s2 = new Student('Bob', 22, 2.57)
 let s3 = new Student('Craft', 43, 4.00)
 
-// class method
+// class/static method
 Student.showTotalNumberOfStudents()
 
-// name
+// instance methods
 s1.getName()
 s2.getName()
 s3.getName()
-
-// age
-s1.getAge()
-s2.getAge()
-s3.getAge()
