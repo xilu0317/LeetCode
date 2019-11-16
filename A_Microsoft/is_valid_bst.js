29de5814c452eaefdbbe74a7cec32faf484a8759
Xi Lu <xilu0317@gmail.com>
2019-11-16 14:09:58 -0800

    new

--- A_Microsoft/is_valid_bst.js
+++ A_Microsoft/is_valid_bst.js
@@ -1,42 +1,8 @@
-const getMax = (root) => {
-    if (!root) return null
-
-    // keep going right as long as right exists
-    while (root.right) {
-        root = root.right
-    }
-
-    return root.val
-}
-
-const getMin = (root) => {
-    if (!root) return null
-
-    while (root.left) {
-        root = root.left
-    }
-
-    return root.val
-}
-
-const isValidBST = (root) => {
-    // per spec
+const isValidBST = (root, lessThan = Infinity, largerThan = -Infinity) => {
     if (!root) return true
 
-    // neither exists
-    if (!root.left && !root.right) return true
+    if (root.val <= largerThan || root.val >= lessThan) return false
 
-    // left doesn't exist, cur val must be smaller than the smallest value of right subtree
-    if (!root.left)
-        return isValidBST(root.right) && root.val < getMin(root.right)
-
-    // right doesn't exist, cur val must be larger than the largest value of left subtree
-    if (!root.right)
-        return isValidBST(root.left) && root.val > getMax(root.left)
-
-    // both exists
-    return isValidBST(root.left) &&
-           isValidBST(root.right) &&
-           root.val > getMax(root.left) &&
-           root.val < getMin(root.right)
+    return isValidBST(root.left, Math.min(root.val, lessThan), largerThan) &&
+           isValidBST(root.right, lessThan, Math.max(root.val, largerThan))
 }
