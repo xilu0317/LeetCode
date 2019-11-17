0dd040f1dc3df9b8f14ef77a1ef0466c4002157a
Xi Lu <xilu0317@gmail.com>
2019-11-16 20:46:11 -0800

    new

--- FE_engineering/promise_and_fetch/promise_sequencing.js
+++
@@ -1,5 +0,0 @@
-// use reduce for example
-
-// https://hackernoon.com/functional-javascript-resolving-promises-sequentially-7aac18c4431e
-// https://css-tricks.com/why-using-reduce-to-sequentially-resolve-promises-works/
-// https://decembersoft.com/posts/promises-in-serial-with-array-reduce/
